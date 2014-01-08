bf6a0eb37e3e5f6d7e657579e0a2bffa0e00d1db
Jakob Glen Homan <jghoman@apache.org>
2014-01-08 13:39:05 -0800

    SAMZA-118: Change Partition hashcode to just the partition number.

--- samza-api/src/main/java/org/apache/samza/Partition.java
+++ samza-api/src/main/java/org/apache/samza/Partition.java
@@ -32,10 +32,7 @@ public class Partition {
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + partition;
-    return result;
+    return partition;
   }
 
   @Override
