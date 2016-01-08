104491c13383024dbe0abd2600aebf808179c124
Vadim Chekan <kot.begemot@gmail.com>
2016-01-07 16:25:13 -0800

    Inability to create configured producer should be logged as error.
    Don't drop original exception.

--- samza-core/src/main/scala/org/apache/samza/container/SamzaContainer.scala
+++ samza-core/src/main/scala/org/apache/samza/container/SamzaContainer.scala
@@ -195,8 +195,7 @@ object SamzaContainer extends Logging {
             (systemName, systemFactory.getProducer(systemName, config, samzaContainerMetrics.registry))
           } catch {
             case e: Exception =>
-              info("Failed to create a producer for %s, so skipping." format systemName)
-              debug("Exception detail:", e)
+              error("Failed to create a producer for %s, so skipping.".format(systemName), e)
               (systemName, null)
           }
       }
