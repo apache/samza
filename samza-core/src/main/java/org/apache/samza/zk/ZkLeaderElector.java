d4ed6f13eff4e59896011839d4851fbf9760201c
navina <navina@apache.org>
2017-01-23 13:16:49 -0800

    Extracting files related to LeaderElection

---
+++ samza-core/src/main/java/org/apache/samza/zk/ZkLeaderElector.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.zk;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.apache.samza.SamzaException;
+import org.apache.samza.coordinator.leaderelection.LeaderElector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class ZkLeaderElector implements LeaderElector {
+  public static final Logger log = LoggerFactory.getLogger(ZkLeaderElector.class);
+  private final ZkUtils zkUtils;
+  private final String processorIdStr;
+  private final ZkKeyBuilder keyBuilder;
+
+  private String leaderId = null;
+  private final ZkLeaderListener zkLeaderListener = new ZkLeaderListener();
+  private String currentSubscription = null;
+  private final Random random = new Random();
+
+  public ZkLeaderElector(String processorIdStr, ZkUtils zkUtils) {
+    this.processorIdStr = processorIdStr;
+    this.zkUtils = zkUtils;
+    this.keyBuilder = this.zkUtils.getKeyBuilder();
+  }
+
+  @Override
+  public boolean tryBecomeLeader() {
+    String currentPath = zkUtils.getEphemeralPath();
+
+    if (currentPath == null || currentPath.isEmpty()) {
+      zkUtils.registerProcessorAndGetId();
+      currentPath = zkUtils.getEphemeralPath();
+    }
+
+    List<String> children = zkUtils.getActiveProcessors();
+    int index = children.indexOf(ZkKeyBuilder.parseIdFromPath(currentPath));
+
+    if (index == -1) {
+      // Retry register here??
+      throw new SamzaException("Looks like we are no longer connected to Zk. Need to reconnect??");
+    }
+
+    if (index == 0) {
+      log.info("pid=" + processorIdStr + " Eligible to be the leader!");
+      leaderId = ZkKeyBuilder.parseIdFromPath(currentPath);
+      return true;
+    }
+
+    log.info("pid=" + processorIdStr + ";index=" + index + ";children=" + Arrays.toString(children.toArray()) + " Not eligible to be a leader yet!");
+    leaderId = ZkKeyBuilder.parseIdFromPath(children.get(0));
+    String prevCandidate = children.get(index - 1);
+    if (!prevCandidate.equals(currentSubscription)) {
+      if (currentSubscription != null) {
+        zkUtils.unsubscribeDataChanges(keyBuilder.getProcessorsPath() + "/" + currentSubscription, zkLeaderListener);
+      }
+      currentSubscription = prevCandidate;
+      log.info("pid=" + processorIdStr + "Subscribing to " + prevCandidate);
+      zkUtils.subscribeDataChanges(keyBuilder.getProcessorsPath() + "/" + currentSubscription, zkLeaderListener);
+    }
+
+    // Double check that the previous candidate still exists
+    boolean prevCandidateExists = zkUtils.exists(keyBuilder.getProcessorsPath() + "/" + currentSubscription);
+    if (prevCandidateExists) {
+      log.info("pid=" + processorIdStr + "Previous candidate still exists. Continuing as non-leader");
+    } else {
+      // TODO - what actually happens here..
+      try {
+        Thread.sleep(random.nextInt(1000));
+      } catch (InterruptedException e) {
+        Thread.interrupted();
+      }
+      log.info("pid=" + processorIdStr + "Previous candidate doesn't exist anymore. Trying to become leader again...");
+      return tryBecomeLeader();
+    }
+    return false;
+  }
+
+  @Override
+  public void resignLeadership() {
+
+  }
+
+  @Override
+  public boolean amILeader() {
+    return zkUtils.getEphemeralPath() != null
+        && leaderId != null
+        && leaderId.equals(ZkKeyBuilder.parseIdFromPath(zkUtils.getEphemeralPath()));
+  }
+
+  // Only by non-leaders
+  class ZkLeaderListener implements IZkDataListener {
+
+    @Override
+    public void handleDataChange(String dataPath, Object data) throws Exception {
+      log.debug("ZkLeaderListener::handleDataChange on path " + dataPath + " Data: " + data);
+    }
+
+    @Override
+    public void handleDataDeleted(String dataPath) throws Exception {
+      log.info("ZkLeaderListener::handleDataDeleted on path " + dataPath);
+      tryBecomeLeader();
+    }
+  }
+}
---
+++ samza-core/src/main/java/org/apache/samza/zk/ZkUtils.java
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.zk;
+
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.IZkStateListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+import org.I0Itec.zkclient.exception.ZkInterruptedException;
+import org.apache.zookeeper.Watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ZkUtils {
+  private static final Logger LOG = LoggerFactory.getLogger(ZkUtils.class);
+
+  private final ZkStateChangeHandler zkStateChangeHandler;
+  private final ZkClient zkClient;
+  private final ZkConnection zkConnnection;
+  private volatile String ephemeralPath = null;
+  private final ZkKeyBuilder keyBuilder;
+  private final int connectionTimeoutMs;
+  private final String processorId;
+
+  public ZkUtils(ZkKeyBuilder zkKeyBuilder, String zkConnectString, String processorId, int sessionTimeoutMs, int connectionTimeoutMs) {
+    this.keyBuilder = zkKeyBuilder;
+    this.connectionTimeoutMs = connectionTimeoutMs;
+    this.zkConnnection = new ZkConnection(zkConnectString, sessionTimeoutMs);
+    this.zkClient = new ZkClient(zkConnnection, this.connectionTimeoutMs);
+    this.zkStateChangeHandler = new ZkStateChangeHandler();
+    this.processorId = processorId;
+  }
+
+  public void connect() throws ZkInterruptedException {
+    boolean isConnected = zkClient.waitUntilConnected(connectionTimeoutMs, TimeUnit.MILLISECONDS);
+    if (!isConnected) {
+      throw new RuntimeException("Unable to connect to Zookeeper within connectionTimeout " + connectionTimeoutMs + "ms. Shutting down!");
+    } else {
+      zkClient.subscribeStateChanges(zkStateChangeHandler);
+    }
+  }
+
+  public ZkClient getZkClient() {
+    return zkClient;
+  }
+
+  public ZkConnection getZkConnnection() {
+    return zkConnnection;
+  }
+
+  public ZkKeyBuilder getKeyBuilder() {
+    return keyBuilder;
+  }
+
+  public void makeSurePersistentPathsExists(String[] paths) {
+    for (String path : paths) {
+      if (!zkClient.exists(path)) {
+        zkClient.createPersistent(path, true);
+      }
+    }
+  }
+
+  public synchronized String registerProcessorAndGetId() {
+    try {
+      // TODO: Data should be more than just the hostname. Use Json serialized data
+      ephemeralPath =
+          zkClient.createEphemeralSequential(keyBuilder.getProcessorsPath() + "/processor-", InetAddress.getLocalHost().getHostName());
+      return ephemeralPath;
+    } catch (UnknownHostException e) {
+      throw new RuntimeException("Failed to register as worker. Aborting...");
+    }
+  }
+
+  public synchronized String getEphemeralPath() {
+    return ephemeralPath;
+  }
+
+  public List<String> getActiveProcessors() {
+    List<String> children = zkClient.getChildren(keyBuilder.getProcessorsPath());
+    assert children.size() > 0;
+    Collections.sort(children);
+    LOG.info("Found these children - " + children);
+    return children;
+  }
+
+  public void subscribeToProcessorChange(IZkChildListener listener) {
+    LOG.info("pid=" + processorId + " subscribing for child change at:" + keyBuilder.getProcessorsPath());
+    zkClient.subscribeChildChanges(keyBuilder.getProcessorsPath(), listener);
+  }
+
+  /* Wrapper for standard I0Itec methods */
+  public void unsubscribeDataChanges(String path, IZkDataListener dataListener) {
+    LOG.info("pid=" + processorId + " unsubscribing for data change at:" + path);
+    zkClient.unsubscribeDataChanges(path, dataListener);
+  }
+
+  public void subscribeDataChanges(String path, IZkDataListener dataListener) {
+    LOG.info("pid=" + processorId + " subscribing for data change at:" + path);
+    zkClient.subscribeDataChanges(path, dataListener);
+  }
+
+  public boolean exists(String path) {
+    return zkClient.exists(path);
+  }
+
+  public void close() {
+    try {
+      zkConnnection.close();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    zkClient.close();
+  }
+
+  class ZkStateChangeHandler implements IZkStateListener {
+    public ZkStateChangeHandler() {
+    }
+
+    /**
+     * Called when the zookeeper connection state has changed.
+     *
+     * @param state The new state.
+     * @throws Exception On any error.
+     */
+    @Override
+    public void handleStateChanged(Watcher.Event.KeeperState state) throws Exception {
+    }
+
+    /**
+     * Called after the zookeeper session has expired and a new session has been created. You would have to re-create
+     * any ephemeral nodes here.
+     *
+     * @throws Exception On any error.
+     */
+    @Override
+    public void handleNewSession() throws Exception {
+
+    }
+
+    /**
+     * Called when a session cannot be re-established. This should be used to implement connection
+     * failure handling e.g. retry to connect or pass the error up
+     *
+     * @param error The error that prevents a session from being established
+     * @throws Exception On any error.
+     */
+    @Override
+    public void handleSessionEstablishmentError(Throwable error) throws Exception {
+
+    }
+  }
+}
