38b1dc38dfc0b71df5dd5ca983c4c668a6cc20be
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2017-02-08 16:07:27 -0800

    SAMZA-1083 Do not load task stores which are older than delete tombstones during container startup

--- samza-core/src/main/java/org/apache/samza/storage/StorageRecovery.java
+++ samza-core/src/main/java/org/apache/samza/storage/StorageRecovery.java
@@ -30,6 +30,7 @@ import org.apache.samza.SamzaException;
 import org.apache.samza.config.Config;
 import org.apache.samza.config.JavaStorageConfig;
 import org.apache.samza.config.JavaSystemConfig;
+import org.apache.samza.config.StorageConfig;
 import org.apache.samza.container.SamzaContainerContext;
 import org.apache.samza.coordinator.JobModelManager;
 import org.apache.samza.job.model.ContainerModel;
@@ -238,7 +239,6 @@ public class StorageRecovery extends CommandLine {
             taskStores.put(storeName, storageEngine);
           }
         }
-
         TaskStorageManager taskStorageManager = new TaskStorageManager(
             taskModel.getTaskName(),
             Util.javaMapAsScalaMap(taskStores),
@@ -247,8 +247,11 @@ public class StorageRecovery extends CommandLine {
             maxPartitionNumber,
             streamMetadataCache,
             storeBaseDir,
-            storeBaseDir, taskModel.getChangelogPartition(),
-            Util.javaMapAsScalaMap(systemAdmins));
+            storeBaseDir,
+            taskModel.getChangelogPartition(),
+            Util.javaMapAsScalaMap(systemAdmins),
+            new StorageConfig(jobConfig).getChangeLogDeleteRetentionsInMs(),
+            new SystemClock());
 
         taskStorageManagers.add(taskStorageManager);
       }
