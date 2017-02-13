2226e3e710171336e0904c5a097d8ac8fff9f517
navina <navina@apache.org>
2017-02-13 13:32:06 -0800

    SAMZA-1082 : Implement Leader Election using ZK
    Simple implementation of leader election recipe along with unit tests
    
    Author: navina <navina@apache.org>
    
    Reviewers: Xinyu Liu <xinyuliu.us@gmail.com>, Fred Ji <fji@linkedin.com>
    
    Closes #48 from navina/LeaderElector

---
+++ samza-core/src/test/java/org/apache/samza/zk/TestZkLeaderElector.java
@@ -0,0 +1,405 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.samza.zk;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkConnection;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.apache.samza.SamzaException;
+import org.apache.samza.testUtils.EmbeddedZookeeper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestZkLeaderElector {
+
+  private static EmbeddedZookeeper zkServer = null;
+  private static final ZkKeyBuilder KEY_BUILDER = new ZkKeyBuilder("test");
+  private String testZkConnectionString = null;
+  private ZkUtils testZkUtils = null;
+  private static final int SESSION_TIMEOUT_MS = 20000;
+  private static final int CONNECTION_TIMEOUT_MS = 10000;
+
+  @BeforeClass
+  public static void setup() throws InterruptedException {
+    zkServer = new EmbeddedZookeeper();
+    zkServer.setup();
+  }
+
+  @Before
+  public void testSetup() {
+    testZkConnectionString = "localhost:" + zkServer.getPort();
+    try {
+      testZkUtils = getZkUtilsWithNewClient();
+    } catch (Exception e) {
+      Assert.fail("Client connection setup failed. Aborting tests..");
+    }
+    try {
+      testZkUtils.getZkClient().createPersistent(KEY_BUILDER.getProcessorsPath(), true);
+    } catch (ZkNodeExistsException e) {
+      // Do nothing
+    }
+  }
+
+
+  @After
+  public void testTeardown() {
+    testZkUtils.close();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    zkServer.teardown();
+  }
+
+  @Test
+  public void testLeaderElectionRegistersProcessor() {
+    List<String> activeProcessors = new ArrayList<String>() {
+      {
+        add("0000000000");
+      }
+    };
+
+    ZkUtils mockZkUtils = mock(ZkUtils.class);
+    when(mockZkUtils.registerProcessorAndGetId(any())).
+        thenReturn(KEY_BUILDER.getProcessorsPath() + "/0000000000");
+    when(mockZkUtils.getSortedActiveProcessors()).thenReturn(activeProcessors);
+
+    ZkLeaderElector leaderElector = new ZkLeaderElector("1", mockZkUtils);
+    Assert.assertTrue(leaderElector.tryBecomeLeader());
+  }
+
+  @Test
+  public void testUnregisteredProcessorInLeaderElection() {
+    String processorId = "1";
+    ZkUtils mockZkUtils = mock(ZkUtils.class);
+    when(mockZkUtils.getSortedActiveProcessors()).thenReturn(new ArrayList<String>());
+
+    ZkLeaderElector leaderElector = new ZkLeaderElector(processorId, mockZkUtils);
+    try {
+      leaderElector.tryBecomeLeader();
+      Assert.fail("Was expecting leader election to fail!");
+    } catch (SamzaException e) {
+      // No-op Expected
+    }
+  }
+
+  /**
+   * Test starts 3 processors and verifies the state of the Zk tree after all processors participate in LeaderElection
+   */
+  @Test
+  public void testLeaderElection() {
+    // Processor-1
+    ZkUtils zkUtils1 = getZkUtilsWithNewClient();
+    ZkLeaderElector leaderElector1 = new ZkLeaderElector(
+        "1",
+        zkUtils1);
+
+    // Processor-2
+    ZkUtils zkUtils2 = getZkUtilsWithNewClient();
+    ZkLeaderElector leaderElector2 = new ZkLeaderElector(
+        "2",
+        zkUtils2);
+
+    // Processor-3
+    ZkUtils zkUtils3  = getZkUtilsWithNewClient();
+    ZkLeaderElector leaderElector3 = new ZkLeaderElector(
+        "3",
+        zkUtils3);
+
+    Assert.assertEquals(0, testZkUtils.getSortedActiveProcessors().size());
+
+    Assert.assertTrue(leaderElector1.tryBecomeLeader());
+    Assert.assertFalse(leaderElector2.tryBecomeLeader());
+    Assert.assertFalse(leaderElector3.tryBecomeLeader());
+
+    Assert.assertEquals(3, testZkUtils.getSortedActiveProcessors().size());
+
+    // Clean up
+    zkUtils1.close();
+    zkUtils2.close();
+    zkUtils3.close();
+
+    Assert.assertEquals(new ArrayList<String>(), testZkUtils.getSortedActiveProcessors());
+
+  }
+
+  /**
+   * Tests that Leader Failure automatically promotes the next successor to become the leader
+   */
+  @Test
+  public void testLeaderFailure() {
+    /**
+     * electionLatch and count together verify that:
+     * 1. the registered listeners are actually invoked by the ZkClient on the correct path
+     * 2. for a single participant failure, at-most 1 other participant is notified
+     */
+    final CountDownLatch electionLatch = new CountDownLatch(1);
+    final AtomicInteger count = new AtomicInteger(0);
+
+    // Processor-1
+    ZkUtils zkUtils1 = getZkUtilsWithNewClient();
+    zkUtils1.registerProcessorAndGetId("processor1");
+    ZkLeaderElector leaderElector1 = new ZkLeaderElector(
+        "1",
+        zkUtils1,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            count.incrementAndGet();
+          }
+        });
+
+
+    // Processor-2
+    ZkUtils zkUtils2 = getZkUtilsWithNewClient();
+    final String path2 = zkUtils2.registerProcessorAndGetId("processor2");
+    ZkLeaderElector leaderElector2 = new ZkLeaderElector(
+        "2",
+        zkUtils2,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            String registeredIdStr = ZkKeyBuilder.parseIdFromPath(path2);
+            Assert.assertNotNull(registeredIdStr);
+
+            String predecessorIdStr = ZkKeyBuilder.parseIdFromPath(dataPath);
+            Assert.assertNotNull(predecessorIdStr);
+
+            try {
+              int selfId = Integer.parseInt(registeredIdStr);
+              int predecessorId = Integer.parseInt(predecessorIdStr);
+              Assert.assertEquals(1, selfId - predecessorId);
+            } catch (Exception e) {
+              System.out.println(e.getMessage());
+            }
+            count.incrementAndGet();
+            electionLatch.countDown();
+          }
+        });
+
+    // Processor-3
+    ZkUtils zkUtils3  = getZkUtilsWithNewClient();
+    zkUtils3.registerProcessorAndGetId("processor3");
+    ZkLeaderElector leaderElector3 = new ZkLeaderElector(
+        "3",
+        zkUtils3,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            count.incrementAndGet();
+          }
+        });
+
+    // Join Leader Election
+    Assert.assertTrue(leaderElector1.tryBecomeLeader());
+    Assert.assertFalse(leaderElector2.tryBecomeLeader());
+    Assert.assertFalse(leaderElector3.tryBecomeLeader());
+
+    Assert.assertTrue(leaderElector1.amILeader());
+    Assert.assertFalse(leaderElector2.amILeader());
+    Assert.assertFalse(leaderElector3.amILeader());
+
+    List<String> currentActiveProcessors = testZkUtils.getSortedActiveProcessors();
+    Assert.assertEquals(3, currentActiveProcessors.size());
+
+    // Leader Failure
+    zkUtils1.close();
+    currentActiveProcessors.remove(0);
+
+    try {
+      Assert.assertTrue(electionLatch.await(5, TimeUnit.SECONDS));
+    } catch (InterruptedException e) {
+      Assert.fail("Interrupted while waiting for leaderElection listener callback to complete!");
+    }
+
+    Assert.assertEquals(1, count.get());
+    Assert.assertEquals(currentActiveProcessors, testZkUtils.getSortedActiveProcessors());
+
+    // Clean up
+    zkUtils2.close();
+    zkUtils3.close();
+  }
+
+  /**
+   * Tests that a non-leader failure updates the Zk tree and participants' state correctly
+   */
+  @Test
+  public void testNonLeaderFailure() {
+    /**
+     * electionLatch and count together verify that:
+     * 1. the registered listeners are actually invoked by the ZkClient on the correct path
+     * 2. for a single participant failure, at-most 1 other participant is notified
+     */
+    final CountDownLatch electionLatch = new CountDownLatch(1);
+    final AtomicInteger count = new AtomicInteger(0);
+
+    // Processor-1
+    ZkUtils zkUtils1 = getZkUtilsWithNewClient();
+    zkUtils1.registerProcessorAndGetId("processor1");
+    ZkLeaderElector leaderElector1 = new ZkLeaderElector(
+        "1",
+        zkUtils1,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            count.incrementAndGet();
+          }
+        });
+
+    // Processor-2
+    ZkUtils zkUtils2 = getZkUtilsWithNewClient();
+    zkUtils2.registerProcessorAndGetId("processor2");
+    ZkLeaderElector leaderElector2 = new ZkLeaderElector(
+        "2",
+        zkUtils2,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            count.incrementAndGet();
+          }
+        });
+
+    // Processor-3
+    ZkUtils zkUtils3  = getZkUtilsWithNewClient();
+    final String path3 = zkUtils3.registerProcessorAndGetId("processor3");
+    ZkLeaderElector leaderElector3 = new ZkLeaderElector(
+        "3",
+        zkUtils3,
+        new IZkDataListener() {
+          @Override
+          public void handleDataChange(String dataPath, Object data) throws Exception {
+
+          }
+
+          @Override
+          public void handleDataDeleted(String dataPath) throws Exception {
+            String registeredIdStr = ZkKeyBuilder.parseIdFromPath(path3);
+            Assert.assertNotNull(registeredIdStr);
+
+            String predecessorIdStr = ZkKeyBuilder.parseIdFromPath(dataPath);
+            Assert.assertNotNull(predecessorIdStr);
+
+            try {
+              int selfId = Integer.parseInt(registeredIdStr);
+              int predecessorId = Integer.parseInt(predecessorIdStr);
+              Assert.assertEquals(1, selfId - predecessorId);
+            } catch (Exception e) {
+              Assert.fail("Exception in LeaderElectionListener!");
+            }
+            count.incrementAndGet();
+            electionLatch.countDown();
+          }
+        });
+
+    // Join Leader Election
+    Assert.assertTrue(leaderElector1.tryBecomeLeader());
+    Assert.assertFalse(leaderElector2.tryBecomeLeader());
+    Assert.assertFalse(leaderElector3.tryBecomeLeader());
+
+    List<String> currentActiveProcessors = testZkUtils.getSortedActiveProcessors();
+    Assert.assertEquals(3, currentActiveProcessors.size());
+
+    zkUtils2.close();
+    currentActiveProcessors.remove(1);
+
+    try {
+      Assert.assertTrue(electionLatch.await(5, TimeUnit.SECONDS));
+    } catch (InterruptedException e) {
+      Assert.fail("Interrupted while waiting for leaderElection listener callback to complete!");
+    }
+
+    Assert.assertEquals(1, count.get());
+    Assert.assertEquals(currentActiveProcessors, testZkUtils.getSortedActiveProcessors());
+
+    // Clean up
+    zkUtils1.close();
+    zkUtils3.close();
+  }
+
+  @Test
+  public void testAmILeader() {
+    // Processor-1
+    ZkLeaderElector leaderElector1 = new ZkLeaderElector(
+        "1",
+        getZkUtilsWithNewClient());
+
+    // Processor-2
+    ZkLeaderElector leaderElector2 = new ZkLeaderElector(
+        "2",
+        getZkUtilsWithNewClient());
+
+    // Before Leader Election
+    Assert.assertFalse(leaderElector1.amILeader());
+    Assert.assertFalse(leaderElector2.amILeader());
+
+    leaderElector1.tryBecomeLeader();
+    leaderElector2.tryBecomeLeader();
+
+    // After Leader Election
+    Assert.assertTrue(leaderElector1.amILeader());
+    Assert.assertFalse(leaderElector2.amILeader());
+  }
+
+  private ZkUtils getZkUtilsWithNewClient() {
+    ZkConnection zkConnection = ZkUtils.createZkConnection(testZkConnectionString, SESSION_TIMEOUT_MS);
+    return new ZkUtils(
+        KEY_BUILDER,
+        ZkUtils.createZkClient(zkConnection, CONNECTION_TIMEOUT_MS),
+        CONNECTION_TIMEOUT_MS);
+  }
+}
---
+++ samza-core/src/test/java/org/apache/samza/zk/TestZkUtils.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.samza.zk;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.apache.samza.testUtils.EmbeddedZookeeper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestZkUtils {
+  private static EmbeddedZookeeper zkServer = null;
+  private static final ZkKeyBuilder KEY_BUILDER = new ZkKeyBuilder("test");
+  private ZkConnection zkConnection = null;
+  private ZkClient zkClient = null;
+  private static final int SESSION_TIMEOUT_MS = 20000;
+  private static final int CONNECTION_TIMEOUT_MS = 10000;
+
+  @BeforeClass
+  public static void setup() throws InterruptedException {
+    zkServer = new EmbeddedZookeeper();
+    zkServer.setup();
+  }
+
+  @Before
+  public void testSetup() {
+    try {
+      zkClient = new ZkClient(
+          new ZkConnection("localhost:" + zkServer.getPort(), SESSION_TIMEOUT_MS),
+          CONNECTION_TIMEOUT_MS);
+    } catch (Exception e) {
+      Assert.fail("Client connection setup failed. Aborting tests..");
+    }
+    try {
+      zkClient.createPersistent(KEY_BUILDER.getProcessorsPath(), true);
+    } catch (ZkNodeExistsException e) {
+      // Do nothing
+    }
+  }
+
+
+  @After
+  public void testTeardown() {
+    zkClient.close();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    zkServer.teardown();
+  }
+
+  @Test
+  public void testRegisterProcessorId() {
+    ZkUtils utils = new ZkUtils(
+        KEY_BUILDER,
+        zkClient,
+        SESSION_TIMEOUT_MS);
+    utils.connect();
+    String assignedPath = utils.registerProcessorAndGetId("0.0.0.0");
+    Assert.assertTrue(assignedPath.startsWith(KEY_BUILDER.getProcessorsPath()));
+
+    // Calling registerProcessorId again should return the same ephemeralPath as long as the session is valid
+    Assert.assertTrue(utils.registerProcessorAndGetId("0.0.0.0").equals(assignedPath));
+
+    utils.close();
+  }
+
+  @Test
+  public void testGetActiveProcessors() {
+    ZkUtils utils = new ZkUtils(
+        KEY_BUILDER,
+        zkClient,
+        SESSION_TIMEOUT_MS);
+    utils.connect();
+
+    Assert.assertEquals(0, utils.getSortedActiveProcessors().size());
+    utils.registerProcessorAndGetId("processorData");
+
+    Assert.assertEquals(1, utils.getSortedActiveProcessors().size());
+
+    utils.close();
+  }
+
+}
