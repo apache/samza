520a08385799038f002ff6d1fc0897053a21b857
Boris Shkolnik <boryas@apache.org>
2017-03-02 11:35:50 -0800

    added ZKJobCoordinatorFactory

---
+++ samza-core/src/main/java/org/apache/samza/zk/ZkJobCoordinatorFactory.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.zk;
+
+import org.apache.samza.config.Config;
+import org.apache.samza.config.JavaJobConfig;
+import org.apache.samza.config.ZkConfig;
+import org.apache.samza.coordinator.JobCoordinator;
+import org.apache.samza.coordinator.JobCoordinatorFactory;
+import org.apache.samza.processor.SamzaContainerController;
+
+public class ZkJobCoordinatorFactory implements JobCoordinatorFactory {
+  /**
+   * Method to instantiate an implementation of JobCoordinator
+   *
+   * @param processorId Indicates the StreamProcessor's id to which this Job Coordinator is associated with
+   * @param config      Configs relevant for the JobCoordinator TODO: Separate JC related configs into a "JobCoordinatorConfig"
+   * @return An instance of IJobCoordinator
+   */
+  @Override
+  public JobCoordinator getJobCoordinator(int processorId, Config config, SamzaContainerController containerController) {
+    JavaJobConfig jobConfig = new JavaJobConfig(config);
+    String groupName = String.format("%s-%s", jobConfig.getJobName(), jobConfig.getJobId());
+    ZkConfig zkConfig = new ZkConfig(config);
+    ScheduleAfterDebounceTime debounceTimer = new ScheduleAfterDebounceTime();
+    return new ZkJobCoordinator(
+        processorId,
+        config,
+        debounceTimer,
+        new ZkUtils(
+            new ZkKeyBuilder(groupName),
+            zkConfig.getZkConnect(),
+            debounceTimer,
+            String.valueOf(processorId),
+            zkConfig.getZkSessionTimeoutMs(),
+            zkConfig.getZkConnectionTimeoutMs()),
+        containerController);
+  }
+}
