ffd04d9d674437b44d438e3e372aa0cbbf6b7044
Shanthoosh Venkataraman <svenkataraman@linkedin.com>
2017-03-13 12:07:45 -0700

    SAMZA-1100; Exception when using a stream as both bootstrap and broadcast.
    When a task input stream is used as both broadcast and bootstrap stream in a samza job, Bootstrappingchooser marks the stream as bootstrapped when a single task finishes consuming all the SystemStreamPartitions(This happens when all the starting offset for each partition in the input stream is of type upcoming). This patch fixes this, by marking a stream as bootstrapped, only when all the systemStreamPartitions in a input stream is consumed by all the expected tasks.
    
    More details here : https://issues.apache.org/jira/browse/SAMZA-1100
    
    Author: Shanthoosh Venkataraman <svenkataraman@linkedin.com>
    
    Reviewers: Prateek Maheshwari<prateekm@linkedin.com>
    
    Closes #68 from shanthoosh/master

--- samza-core/src/main/scala/org/apache/samza/system/chooser/BootstrappingChooser.scala
+++ samza-core/src/main/scala/org/apache/samza/system/chooser/BootstrappingChooser.scala
@@ -19,18 +19,20 @@
 
 package org.apache.samza.system.chooser
 
-import java.util.concurrent.atomic.AtomicInteger
+import org.apache.samza.system.SystemAdmin
 import org.apache.samza.system.SystemStream
+import org.apache.samza.system.SystemStreamMetadata
 import org.apache.samza.system.SystemStreamPartition
 import org.apache.samza.system.IncomingMessageEnvelope
+import org.apache.samza.SamzaException
 import org.apache.samza.util.Logging
 import org.apache.samza.metrics.MetricsHelper
 import org.apache.samza.metrics.MetricsRegistryMap
 import org.apache.samza.metrics.MetricsRegistry
-import org.apache.samza.system.SystemStreamMetadata
-import scala.collection.JavaConversions._
-import org.apache.samza.SamzaException
 import org.apache.samza.system.SystemStreamMetadata.OffsetType
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 /**
  * BootstrappingChooser is a composable MessageChooser that only chooses
@@ -69,7 +71,13 @@ class BootstrappingChooser(
   /**
    * An object that holds all of the metrics related to bootstrapping.
    */
-  metrics: BootstrappingChooserMetrics = new BootstrappingChooserMetrics) extends MessageChooser with Logging {
+  metrics: BootstrappingChooserMetrics = new BootstrappingChooserMetrics,
+
+  /**
+   * A map from system stream name to SystemAdmin that is used for
+   * offset comparisons.
+   */
+  systemAdmins: Map[String, SystemAdmin] = Map()) extends MessageChooser with Logging {
 
   /**
    * The number of lagging partitions for each SystemStream that's behind.
@@ -91,9 +99,10 @@ class BootstrappingChooser(
     .toSet
 
   /**
-   * Store all the systemStreamPartitions registered
+   * Mapping from the systemStreamPartition to the lowest registered offset.
+   * When multiple offsets are registered for a system stream partition, lowest offset is chosen.
    */
-  var registeredSystemStreamPartitions = Set[SystemStreamPartition]()
+  var registeredSystemStreamPartitions = mutable.Map[SystemStreamPartition, String]()
 
   /**
    * The number of lagging partitions that the underlying wrapped chooser has
@@ -102,8 +111,16 @@ class BootstrappingChooser(
   var updatedSystemStreams = Map[SystemStream, Int]()
 
   def start = {
+    for ((systemStreamPartition, offset) <- registeredSystemStreamPartitions) {
+      // If the offset we're starting to consume from is the same as the upcoming
+      // offset for this system stream partition, then we've already read all
+      // messages in the stream, and we're at head for this system stream
+      // partition.
+      checkOffset(systemStreamPartition, offset, OffsetType.UPCOMING)
+    }
+
     // remove the systemStreamPartitions not registered.
-    laggingSystemStreamPartitions = laggingSystemStreamPartitions.filter(registeredSystemStreamPartitions.contains(_))
+    laggingSystemStreamPartitions = laggingSystemStreamPartitions.filter(registeredSystemStreamPartitions.keys.contains(_))
     systemStreamLagCounts = laggingSystemStreamPartitions.groupBy(_.getSystemStream).map {case (systemStream, ssps) => systemStream -> ssps.size}
 
     debug("Starting bootstrapping chooser with bootstrap metadata: %s" format bootstrapStreamMetadata)
@@ -120,15 +137,27 @@ class BootstrappingChooser(
   override def register(systemStreamPartition: SystemStreamPartition, offset: String) {
     debug("Registering stream partition with offset: %s, %s" format (systemStreamPartition, offset))
 
-    // If the offset we're starting to consume from is the same as the upcoming 
-    // offset for this system stream partition, then we've already read all
-    // messages in the stream, and we're at head for this system stream 
-    // partition.
-    checkOffset(systemStreamPartition, offset, OffsetType.UPCOMING)
-
     wrapped.register(systemStreamPartition, offset)
 
-    registeredSystemStreamPartitions += systemStreamPartition
+    val system = systemStreamPartition.getSystem
+    val systemAdmin = systemAdmins.getOrElse(system, throw new SamzaException("SystemAdmin is undefined for System: %s" format system))
+    /**
+     * SAMZA-1100: When a input SystemStream is consumed as both bootstrap and broadcast
+     * BootstrappingChooser should record the lowest offset for each registered SystemStreamPartition.
+     * When multiple tasks processing a SystemStreamPartition runs within a container
+     * and share the same chooser, then the lowest offset should be chosen as starting offset.
+     */
+    if (!registeredSystemStreamPartitions.contains(systemStreamPartition)) {
+      registeredSystemStreamPartitions += systemStreamPartition -> offset
+    } else if (offset != null) {
+      val existingOffset = registeredSystemStreamPartitions(systemStreamPartition)
+      val comparatorResult: Integer = systemAdmin.offsetComparator(existingOffset, offset)
+      if (comparatorResult == null) {
+        warn("Existing offset: %s and incoming offset: %s of system stream partition: %s are not comparable." format (existingOffset, offset, systemStreamPartition))
+      } else if (comparatorResult > 0) {
+        registeredSystemStreamPartitions += systemStreamPartition -> offset
+      }
+    }
   }
 
   def update(envelope: IncomingMessageEnvelope) {
