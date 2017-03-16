1676eabc21eae2b0d5aa7d22890b1f7eb1cbb441
Xinyu Liu <xiliu@xiliu-ld.linkedin.biz>
2017-03-15 18:27:01 -0700

    SAMZA-1067; Physical execution graph and planner for fluent API
    Initial commit for the physical graph and plan. Design is there: https://issues.apache.org/jira/secure/attachment/12856670/SAMZA-1067.0.pdf.
    
    The commit includes:
    
    1) Physical ProcessorGraph, where each processor represents a physical execution unit (e.g. a job in Yarn).
    2) A planner does the following:
       - create ProcessorGraph from StreamGraph. For this phase, the graph only contains a single node (single stage);
       - figure out the partitions of intermediate topics
       - create the topics
    
    Please note currently the planner is used in the remote runner for now. Further changes/refactoring/cleanup are expected to be integrated with local runner.
    
    Author: Xinyu Liu <xiliu@xiliu-ld.linkedin.biz>
    
    Reviewers: Jagadish Venkatraman <jvenkatraman@linkedin.com>
    
    Closes #75 from xinyuiscool/SAMZA-1067

--- samza-core/src/main/java/org/apache/samza/operators/spec/OperatorSpecs.java
+++ samza-core/src/main/java/org/apache/samza/operators/spec/OperatorSpecs.java
@@ -148,6 +148,7 @@ public class OperatorSpecs {
    *
    * @param sinkFn  the sink function
    * @param stream  the {@link OutputStream} where the message is sent to
+   * @param opId operator ID
    * @param <M>  type of input message
    * @return  the {@link SinkOperatorSpec}
    */
