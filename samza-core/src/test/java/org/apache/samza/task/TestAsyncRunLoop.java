a6204a747c04ac7122fc84aa317cf70f09e9e32a
Jacob Maes <jmaes@linkedin.com>
2017-03-29 13:31:10 -0700

    Also remove Ignore from the TestAsyncRunLoop test, which causes hudson to fail

--- samza-core/src/test/java/org/apache/samza/task/TestAsyncRunLoop.java
+++ samza-core/src/test/java/org/apache/samza/task/TestAsyncRunLoop.java
@@ -584,7 +584,6 @@ public class TestAsyncRunLoop {
   }
 
   @Test
-  @Ignore
   public void testProcessBehaviourWhenAsyncCommitIsEnabled() throws InterruptedException {
     TestTask task0 = new TestTask(true, true, false);
 
