df8594716d5df4e5b09436c6de159bd296a08a63
Boris Shkolnik <boryas@apache.org>
2017-05-02 15:56:46 -0700

    more cleanup

--- samza-test/src/test/java/org/apache/samza/test/processor/TestZkStreamProcessor.java
+++ samza-test/src/test/java/org/apache/samza/test/processor/TestZkStreamProcessor.java
@@ -1,9 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.samza.test.processor;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -12,8 +27,6 @@ import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import jline.ConsoleReader;
-import jline.ConsoleReaderInputStream;
 import kafka.utils.TestUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -29,7 +42,6 @@ import org.apache.samza.processor.StreamProcessorLifecycleListener;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
-import org.apache.samza.task.ClosableTask;
 import org.apache.samza.task.InitableTask;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
@@ -460,21 +472,15 @@ public class TestZkStreamProcessor extends StandaloneIntegrationTestHarness {
 
       @Override
       public void run() {
-        boolean latchResult = false;
         processor.start();
         try {
-          //Thread.sleep(10000);
-          //latchResult = latch.await(100000, TimeUnit.SECONDS);
+          // just wait
           synchronized (this) {
             this.wait(100000);
           }
-          System.out.println("notifed. Release the lock");
+          System.out.println("notifed. Abandon the wait.");
         } catch (InterruptedException e) {
           e.printStackTrace();
-        } finally {
-          //if (!latchResult) {
-          // Assert.fail("StreamTask either failed to process all input or timed out!");
-          //}
         }
         System.out.println("Stopping the processor");
         processor.stop();
