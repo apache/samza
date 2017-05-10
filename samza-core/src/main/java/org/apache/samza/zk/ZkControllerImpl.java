b3ab71d3b34239c4ff10a9b97fb65bcaf49276a0
Boris Shkolnik <boryas@apache.org>
2017-05-10 16:20:12 -0700

    do not process onProcessorChange if list of children is null

--- samza-core/src/main/java/org/apache/samza/zk/ZkControllerImpl.java
+++ samza-core/src/main/java/org/apache/samza/zk/ZkControllerImpl.java
@@ -94,6 +94,12 @@ public class ZkControllerImpl implements ZkController {
      */
     @Override
     public void handleChildChange(String parentPath, List<String> currentChildren) throws Exception {
+      if (currentChildren == null) {
+        // this may happen only in case of some exception in ZK. Exception should be visible in the logs.
+        // it makes no sense to pass it further down.
+        LOG.error("handleChildChange on path " + parentPath + " was invoked with NULL list of children");
+        return;
+      }
       LOG.info(
           "ZkControllerImpl::ProcessorChangeHandler::handleChildChange - Path: " + parentPath + "  Current Children: "
               + currentChildren);
