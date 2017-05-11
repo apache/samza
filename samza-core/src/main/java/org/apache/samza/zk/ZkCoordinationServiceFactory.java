8d16bbd68f430570926543d54d407c423eb0969d
Boris Shkolnik <boryas@apache.org>
2017-05-11 12:23:56 -0700

    imports

--- samza-core/src/main/java/org/apache/samza/zk/ZkCoordinationServiceFactory.java
+++ samza-core/src/main/java/org/apache/samza/zk/ZkCoordinationServiceFactory.java
@@ -18,14 +18,12 @@
  */
 package org.apache.samza.zk;
 
-import org.I0Itec.zkclient.IZkStateListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.samza.SamzaException;
 import org.apache.samza.config.Config;
 import org.apache.samza.config.ZkConfig;
-import org.apache.samza.coordinator.CoordinationUtils;
 import org.apache.samza.coordinator.CoordinationServiceFactory;
-import org.apache.zookeeper.Watcher;
+import org.apache.samza.coordinator.CoordinationUtils;
 
 
 public class ZkCoordinationServiceFactory implements CoordinationServiceFactory {
