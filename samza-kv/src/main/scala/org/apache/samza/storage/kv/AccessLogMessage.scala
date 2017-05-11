014a59c68b792c8e63639c6f16257c41f0f4e0c6
jmehar2 <jmehar2@illinois.edu>
2017-05-11 12:56:31 -0700

    SAMZA-1174; Profiling state store performance
    This is s commit for [SAMZA-1174](https://issues.apache.org/jira/browse/SAMZA-1174). This commit involves gathering a log of operations (read, write, delete, etc.) happening on the state and publishing them into a kafka topic. It is names as "Access Log" behaving similar to changelog, but gathering log information.
    
    Author: jmehar2 <jmehar2@illinois.edu>
    Author: Jayasi Mehar <jayasi05@gmail.com>
    Author: s-noghabi <abdolla2@illinois.edu>
    
    Reviewers: Prateek Maheshwari <pmaheshw@linkedin.com>, Jagadish V <jagadish@apache.org>
    
    Closes #132 from s-noghabi/master

---
+++ samza-kv/src/main/scala/org/apache/samza/storage/kv/AccessLogMessage.scala
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.storage.kv
+
+import java.io.Serializable
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import java.util.ArrayList
+
+class AccessLogMessage(val DBOperation: Int,
+    val duration: Long,
+    val keys: ArrayList[Array[Byte]],
+    val timestamp: Long = System.currentTimeMillis()
+   ) extends Serializable {
+
+
+  def serialize() : Array[Byte] = {
+    val byteStream = new ByteArrayOutputStream()
+    val outputStream = new ObjectOutputStream(byteStream)
+    outputStream.writeObject(this)
+    outputStream.close
+    val obj: Array[Byte] = byteStream.toByteArray
+    byteStream.close
+    return obj
+  }
+
+}
