b4fea9d3268622c1cdcabb2e1816264aab547b9b
Boris Shkolnik <boryas@apache.org>
2017-06-29 12:10:23 -0700

    checkstyle

--- samza-test/src/test/java/org/apache/samza/processor/TestZkStreamProcessor.java
+++ samza-test/src/test/java/org/apache/samza/processor/TestZkStreamProcessor.java
@@ -20,7 +20,6 @@
 package org.apache.samza.processor;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import org.apache.samza.zk.TestZkUtils;
 import org.junit.Assert;
 import org.junit.Test;
