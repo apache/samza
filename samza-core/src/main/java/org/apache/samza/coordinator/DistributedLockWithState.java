ec075bfd2731e429598c20d357cf6c9550ea51f1
Boris Shkolnik <boryas@apache.org>
2017-08-23 11:02:46 -0700

    added Distributed lock

---
+++ samza-core/src/main/java/org/apache/samza/coordinator/DistributedLockWithState.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.coordinator;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+public interface DistributedLockWithState {
+
+  /**
+   * Tries to acquire the lock, but first checks the state. If state is set, returns false.
+   * If set is not set, and lock is acquired - returns true.
+   * Throws TimeOut exception if could not acquire the lock.
+   * @param timeout Duration of lock acquiring timeout.
+   * @param unit Time Unit of the timeout defined above.
+   * @return true if lock is acquired successfully, false if state is set.
+   */
+  boolean lockIfNotSet(long timeout, TimeUnit unit) throws TimeoutException;
+
+  /**
+   * Releases the lock
+   */
+  void unlock();
+
+  /**
+   * perform any required action for closing
+   */
+  void close();
+
+
+}
