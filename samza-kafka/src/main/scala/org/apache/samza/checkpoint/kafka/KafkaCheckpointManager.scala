72a681f717c30fe7e15ddee20c5d0d375a80b1be
Boris S <boryas@apache.org>
2017-10-17 11:50:19 -0700

    revert some changes

--- samza-kafka/src/main/scala/org/apache/samza/checkpoint/kafka/KafkaCheckpointManager.scala
+++ samza-kafka/src/main/scala/org/apache/samza/checkpoint/kafka/KafkaCheckpointManager.scala
@@ -129,6 +129,9 @@ class KafkaCheckpointManager(
     if (taskNamesToOffsets == null) {
       info("No TaskName to checkpoint mapping provided.  Reading for first time.")
       taskNamesToOffsets = readCheckpointsFromLog()
+    } else {
+      info("Already existing checkpoint mapping.  Merging new offsets")
+      taskNamesToOffsets ++= readCheckpointsFromLog()
     }
 
     val checkpoint = taskNamesToOffsets.get(taskName).getOrElse(null)
