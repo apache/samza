43cbb5265c1b0e962ea727f4e942284fcc4d8bd3
xiliu <xiliu@xiliu-ld1.linkedin.biz>
2017-11-15 17:22:02 -0800

    SAMZA-1504: Allow user to register container-level metrics

--- samza-api/src/main/java/org/apache/samza/container/SamzaContainerContext.java
+++ samza-api/src/main/java/org/apache/samza/container/SamzaContainerContext.java
@@ -20,6 +20,7 @@
 package org.apache.samza.container;
 
 import org.apache.samza.config.Config;
+import org.apache.samza.metrics.MetricsRegistry;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -31,10 +32,30 @@ public class SamzaContainerContext {
   public final String id;
   public final Config config;
   public final Collection<TaskName> taskNames;
+  public final MetricsRegistry metricsRegistry;
 
   /**
    * An immutable context object that can passed to tasks to give them information
    * about the container in which they are executing.
+   * @param id The id of the container.
+   * @param config The job configuration.
+   * @param taskNames The set of taskName keys for which this container is responsible.
+   * @param metricsRegistry the {@link MetricsRegistry} for the container metrics
+   */
+  public SamzaContainerContext(
+      String id,
+      Config config,
+      Collection<TaskName> taskNames,
+      MetricsRegistry metricsRegistry) {
+    this.id = id;
+    this.config = config;
+    this.taskNames = Collections.unmodifiableCollection(taskNames);
+    this.metricsRegistry = metricsRegistry;
+  }
+
+  /**
+   * See also {@link SamzaContainerContext#SamzaContainerContext(String, Config, Collection, MetricsRegistry)}.
+   * NOTE: Used for testing only.
    * @param id The id of the container.
    * @param config The job configuration.
    * @param taskNames The set of taskName keys for which this container is responsible.
@@ -43,8 +64,6 @@ public class SamzaContainerContext {
       String id,
       Config config,
       Collection<TaskName> taskNames) {
-    this.id = id;
-    this.config = config;
-    this.taskNames = Collections.unmodifiableCollection(taskNames);
+    this(id, config, taskNames, null);
   }
 }
