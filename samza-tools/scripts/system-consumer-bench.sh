12968cfb6024705d6412b9b9917897f0eb794c3e
Srinivasulu Punuru <spunuru@linkedin.com>
2018-04-17 15:40:59 -0700

    SAMZA-1666: Benchmark SystemProducer and SystemConsumer
    * Tests to benchmark the performance of the system consumers and producers.
    * Config to test the benchmark for the event hub system producer and consumer.
    
    SystemConsumerBench and SystemProducerBench provides base generic implementation to test the benchmark for the system producers and consumers. Any new system that needs benchmark test needs a properties file.
    
    The benchmark test itself is single threaded in the way it consumes and produces events. Scaling the benchmark tests right now involves running multiple processes of these tests in parallel.
    
    Right now we just calculate the event rate, But in future we could create a logging metrics registry to hookup other metrics and log them in console along with event rate while the benchmark tests are being run.
    
    Author: Srinivasulu Punuru <spunuru@linkedin.com>
    
    Reviewers: Jagadish <jagadish@apache.org>, Wei Song<wsong@linkedin.com>
    
    Closes #473 from srinipunuru/benchmark.1

---
+++ samza-tools/scripts/system-consumer-bench.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+if [ `uname` == 'Linux' ];
+then
+  base_dir=$(readlink -f $(dirname $0))
+else
+  base_dir=$(dirname $0)
+fi
+
+if [ "x$LOG4J_OPTS" = "x" ]; then
+    export LOG4J_OPTS="-Dlog4j.configuration=file:$base_dir/../config/bench-log4j.xml"
+fi
+
+if [ "x$HEAP_OPTS" = "x" ]; then
+    export HEAP_OPTS="-Xmx1G -Xms1G"
+fi
+
+exec $base_dir/run-class.sh org.apache.samza.tools.benchmark.SystemConsumerBench "$@"
---
+++ samza-tools/src/main/java/org/apache/samza/tools/benchmark/ConfigBasedSspGrouperFactory.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.tools.benchmark;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.samza.config.Config;
+import org.apache.samza.container.TaskName;
+import org.apache.samza.container.grouper.stream.SystemStreamPartitionGrouper;
+import org.apache.samza.container.grouper.stream.SystemStreamPartitionGrouperFactory;
+import org.apache.samza.system.SystemStream;
+import org.apache.samza.system.SystemStreamPartition;
+
+
+/**
+ * Grouper that assigns only the subset of partitions configured to the task. This can be used only
+ * with {@link org.apache.samza.standalone.PassthroughJobCoordinator}.
+ */
+class ConfigBasedSspGrouperFactory implements SystemStreamPartitionGrouperFactory {
+
+  /**
+   * Comma separated list of partitions that needs to be assigned to this task.
+   */
+  public static final String CONFIG_STREAM_PARTITIONS = "streams.%s.partitions";
+  private static final String CFG_PARTITIONS_DELIMITER = ",";
+
+  @Override
+  public SystemStreamPartitionGrouper getSystemStreamPartitionGrouper(Config config) {
+    return new ConfigBasedSspGrouper(config);
+  }
+
+  private class ConfigBasedSspGrouper implements SystemStreamPartitionGrouper {
+    private final Config config;
+    private HashMap<String, Set<Integer>> _streamPartitionsMap = new HashMap<>();
+
+    public ConfigBasedSspGrouper(Config config) {
+      this.config = config;
+    }
+
+    @Override
+    public Map<TaskName, Set<SystemStreamPartition>> group(Set<SystemStreamPartition> ssps) {
+      Set<SystemStreamPartition> filteredSsps = new HashSet<>();
+      for (SystemStreamPartition ssp : ssps) {
+        Set<Integer> partitions = getPartitions(ssp.getSystemStream());
+        if (partitions.contains(ssp.getPartition().getPartitionId())) {
+          filteredSsps.add(ssp);
+        }
+      }
+      HashMap<TaskName, Set<SystemStreamPartition>> group = new HashMap<>();
+      group.put(new TaskName("TestTask"), filteredSsps);
+      return group;
+    }
+
+    private Set<Integer> getPartitions(SystemStream systemStream) {
+      String streamName = systemStream.getStream();
+
+      if (!_streamPartitionsMap.containsKey(streamName)) {
+        String partitions = config.get(String.format(CONFIG_STREAM_PARTITIONS, streamName));
+        _streamPartitionsMap.put(streamName, Arrays.stream(partitions.split(CFG_PARTITIONS_DELIMITER))
+            .map(Integer::parseInt)
+            .collect(Collectors.toSet()));
+      }
+      return _streamPartitionsMap.get(streamName);
+    }
+  }
+}
