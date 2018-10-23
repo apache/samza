1b2ed8e01023e7eb7e358a5a17f429480d8a7942
Boris S <bshkolnik@linkedin.com>
2018-10-22 17:03:17 -0700

    add hashCode of the object to identify different objects in the logs

--- samza-kafka/src/main/java/org/apache/samza/system/kafka/KafkaSystemConsumer.java
+++ samza-kafka/src/main/java/org/apache/samza/system/kafka/KafkaSystemConsumer.java
@@ -296,7 +296,7 @@ public class KafkaSystemConsumer<K, V> extends BlockingEnvelopeMap implements Sy
 
   @Override
   public String toString() {
-    return String.format("%s:%s", systemName, clientId);
+    return String.format("%s:%s@%s", systemName, clientId, Integer.toHexString(hashCode()));
   }
 
   @Override
