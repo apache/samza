993114bc73601c726f376840642df37d66746424
Aditya Toomula <atoomula@linkedin.com>
2018-10-30 07:41:46 -0700

    Samza-sql: Change Calcite sql type for samza sql rel message __key__ to accept any format.

--- samza-sql/src/main/java/org/apache/samza/sql/avro/AvroRelSchemaProvider.java
+++ samza-sql/src/main/java/org/apache/samza/sql/avro/AvroRelSchemaProvider.java
@@ -25,19 +25,9 @@ import org.apache.samza.system.SystemStream;
 
 public interface AvroRelSchemaProvider extends RelSchemaProvider {
   /**
-   * Get payload schema corresponding to the system stream. Please use {@link #getPayloadSchema} instead.
-   * @param systemStream system stream for which payload schema needs to be obtained.
-   * @return schema in the form of string
-   */
-  @Deprecated
-  String getSchema(SystemStream systemStream);
-
-  /**
    * Get payload schema corresponding to the system stream.
    * @param systemStream system stream for which payload schema needs to be obtained.
    * @return schema in the form of string
    */
-  default String getPayloadSchema(SystemStream systemStream) {
-    return getSchema(systemStream);
-  }
+  String getSchema(SystemStream systemStream);
 }
