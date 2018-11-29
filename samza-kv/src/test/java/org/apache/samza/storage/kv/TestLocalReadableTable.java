5154f9eb8f484126dbd810a350bf15a10645a1f7
Wei Song <wsong@linkedin.com>
2018-11-29 15:31:58 -0800

    SAMZA-2010: Handle null value in LocalReadWriteTable.putAll()
    To be consistent with put(), null values in input should be delete operation
    
    Author: Wei Song <wsong@linkedin.com>
    
    Reviewers: Ahmed Abdul Hamid <ahabdulhamid@linkedin.com>
    
    Closes #827 from weisong44/SAMZA-2010

--- samza-kv/src/test/java/org/apache/samza/storage/kv/TestLocalReadableTable.java
+++ samza-kv/src/test/java/org/apache/samza/storage/kv/TestLocalReadableTable.java
@@ -124,13 +124,13 @@ public class TestLocalReadableTable {
     ReadableTable table = createTable(true);
     table.get("");
     table.getAsync("").get();
-    table.getAll(Collections.emptyList());
-    table.getAllAsync(Collections.emptyList()).get();
+    table.getAll(keys);
+    table.getAllAsync(keys).get();
     verify(metricsRegistry, atLeast(1)).newCounter(anyString(), anyString());
     verify(metricsRegistry, times(0)).newTimer(anyString(), anyString());
     verify(metricsRegistry, times(0)).newGauge(anyString(), any());
     Assert.assertEquals(2, numGets.getCount());
-    Assert.assertEquals(2, numMissedLookups.getCount());
+    Assert.assertEquals(4, numMissedLookups.getCount());
     Assert.assertEquals(2, numGetAlls.getCount());
     Assert.assertEquals(0, getNs.getSnapshot().getAverage(), 0.001);
     Assert.assertEquals(0, getAllNs.getSnapshot().getAverage(), 0.001);
