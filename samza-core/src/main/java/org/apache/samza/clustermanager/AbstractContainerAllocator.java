0a0d0814a5f324503631ea41fe4987c856f8fab3
Jagadish <jvenkatraman@linkedin.com>
2019-03-19 10:50:58 -0700

    SAMZA-2117: Handle race condition in container launch due to incorrect AM accounting
    Steps involved when starting a Samza container:
    1. Issue a request to YARN to launch a container
    2. Record that container as "pending" launch.
    3. Launch callback succeeds on a different thread : The callback looks at the "pending" container and marks it as "running".
    
    A race-condition in the above: If the main thread gets pre-empted between (1) and (2), the callback thread wouldn't see the container state as "pending" - hence, it wouldn't transition it to a "running" state.
    
    This PR fixes it by flipping (1) and (2) - ie., record the intent prior to issuing the launch request.
    Added an unit test - refactored existing tests
    
    Author: Jagadish <jvenkatraman@linkedin.com>
    
    Reviewers: Prateek M<pmaheshw@linkedin.com>
    
    Closes #958 from vjagadish1989/samza-2117

--- samza-core/src/main/java/org/apache/samza/clustermanager/AbstractContainerAllocator.java
+++ samza-core/src/main/java/org/apache/samza/clustermanager/AbstractContainerAllocator.java
@@ -152,10 +152,13 @@ public abstract class AbstractContainerAllocator implements Runnable {
             + "timestamp {} to resource {}",
         new Object[]{preferredHost, String.valueOf(containerID), request.getRequestTimestampMs(), resource.getResourceID()});
 
-    //Submit a request to launch a StreamProcessor on the provided resource. To match with the response returned later
-    //in the callback, we should also store state about the container whose launch is pending.
-    clusterResourceManager.launchStreamProcessor(resource, builder);
+    // Update container state as "pending" and then issue a request to launch it. It's important to perform the state-update
+    // prior to issuing the request. Otherwise, there's a race where the response callback may arrive sooner and not see
+    // the container as "pending" (SAMZA-2117)
+
     state.pendingContainers.put(containerID, resource);
+
+    clusterResourceManager.launchStreamProcessor(resource, builder);
   }
 
   /**
