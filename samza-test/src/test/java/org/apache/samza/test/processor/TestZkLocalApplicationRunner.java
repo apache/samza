apache/samza

commit 01c06055057e8c260e73bf3f7db7b9eb53af3745

SAMZA-2127: Upgrade to Kafka 2.0 (#951)
* Kafka 2.0 upgrade

* Migrated some of tests to use new Java APIs and remove scala code

* Addressed review comments; fixed all the remaining failures

* Remove unused code

* Minor cleanup

samza-test/src/test/java/org/apache/samza/test/processor/TestZkLocalApplicationRunner.java
@@ -30,7 +30,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.UUID;
 import java.util.Objects;
 import java.util.Set;
@@ -38,11 +37,8 @@ import java.util.HashSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
-import kafka.admin.AdminUtils;
-import kafka.admin.RackAwareMode;
-import kafka.utils.TestUtils;
 import org.I0Itec.zkclient.ZkClient;
-import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.samza.Partition;
 import org.apache.samza.application.TaskApplication;
@@ -69,8 +65,8 @@ import org.apache.samza.metrics.MetricsRegistryMap;
 import org.apache.samza.runtime.ApplicationRunner;
 import org.apache.samza.runtime.ApplicationRunners;
 import org.apache.samza.system.SystemStreamPartition;
-import org.apache.samza.test.StandaloneIntegrationTestHarness;
 import org.apache.samza.test.StandaloneTestUtils;
+import org.apache.samza.test.harness.IntegrationTestHarness;
 import org.apache.samza.util.NoOpMetricsRegistry;
 import org.apache.samza.util.Util;
 import org.apache.samza.zk.ZkStringSerializer;
@@ -85,8 +81,8 @@ import org.junit.rules.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 
 /**
  * Integration tests for {@link org.apache.samza.runtime.LocalApplicationRunner} with {@link ZkJobCoordinatorFactory}.
@@ -95,7 +91,7 @@ import static org.junit.Assert.assertTrue;
  * through {@link org.apache.samza.runtime.LocalApplicationRunner} to verify the guarantees made in stand alone execution
  * environment.
  */
-public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarness {
+public class TestZkLocalApplicationRunner extends IntegrationTestHarness {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TestZkLocalApplicationRunner.class);
 
@@ -156,42 +152,25 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
     zkUtils = new ZkUtils(zkKeyBuilder, zkClient, ZK_CONNECTION_TIMEOUT_MS, ZK_SESSION_TIMEOUT_MS, new NoOpMetricsRegistry());
     zkUtils.connect();
 
-    for (String kafkaTopic : ImmutableList.of(inputKafkaTopic, outputKafkaTopic)) {
-      LOGGER.info("Creating kafka topic: {}.", kafkaTopic);
-      TestUtils.createTopic(zkUtils(), kafkaTopic, 5, 1, servers(), new Properties());
-      if (AdminUtils.topicExists(zkUtils(), kafkaTopic)) {
-        LOGGER.info("Topic: {} was created", kafkaTopic);
-      } else {
-        Assert.fail(String.format("Unable to create kafka topic: %s.", kafkaTopic));
-      }
-    }
-    for (String kafkaTopic : ImmutableList.of(inputSinglePartitionKafkaTopic, outputSinglePartitionKafkaTopic)) {
-      LOGGER.info("Creating kafka topic: {}.", kafkaTopic);
-      TestUtils.createTopic(zkUtils(), kafkaTopic, 1, 1, servers(), new Properties());
-      if (AdminUtils.topicExists(zkUtils(), kafkaTopic)) {
-        LOGGER.info("Topic: {} was created", kafkaTopic);
-      } else {
-        Assert.fail(String.format("Unable to create kafka topic: %s.", kafkaTopic));
-      }
-    }
+    List<NewTopic> newTopics =
+        ImmutableList.of(inputKafkaTopic, outputKafkaTopic, inputSinglePartitionKafkaTopic, outputSinglePartitionKafkaTopic)
+            .stream()
+            .map(topic -> new NewTopic(topic, 5, (short) 1))
+            .collect(Collectors.toList());
+
+    assertTrue("Encountered errors during test setup. Failed to create topics.", createTopics(newTopics));
   }
 
+  @Override
   public void tearDown() {
+    deleteTopics(ImmutableList.of(
+        inputKafkaTopic, outputKafkaTopic, inputSinglePartitionKafkaTopic, outputSinglePartitionKafkaTopic));
     SharedContextFactories.clearAll();
-    for (String kafkaTopic : ImmutableList.of(inputKafkaTopic, outputKafkaTopic)) {
-      LOGGER.info("Deleting kafka topic: {}.", kafkaTopic);
-      AdminUtils.deleteTopic(zkUtils(), kafkaTopic);
-    }
-    for (String kafkaTopic : ImmutableList.of(inputSinglePartitionKafkaTopic, outputSinglePartitionKafkaTopic)) {
-      LOGGER.info("Deleting kafka topic: {}.", kafkaTopic);
-      AdminUtils.deleteTopic(zkUtils(), kafkaTopic);
-    }
     zkUtils.close();
     super.tearDown();
   }
 
   private void publishKafkaEvents(String topic, int startIndex, int endIndex, String streamProcessorId) {
-    KafkaProducer producer = getKafkaProducer();
     for (int eventIndex = startIndex; eventIndex < endIndex; eventIndex++) {
       try {
         LOGGER.info("Publish kafka event with index : {} for stream processor: {}.", eventIndex, streamProcessorId);
@@ -704,7 +683,7 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
     Assert.assertEquals(5, ssps.size());
 
     // Increase the partition count of input kafka topic to 100.
-    AdminUtils.addPartitions(zkUtils(), inputKafkaTopic, 100, "", true, RackAwareMode.Enforced$.MODULE$);
+    increasePartitionsTo(inputKafkaTopic, 100);
 
     long jobModelWaitTimeInMillis = 10;
     while (Objects.equals(zkUtils.getJobModelVersion(), jobModelVersion)) {
@@ -756,7 +735,7 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
   public void testStatefulSamzaApplicationShouldRedistributeInputPartitionsToCorrectTasksWhenAInputStreamIsExpanded() throws Exception {
     // Setup input topics.
     String statefulInputKafkaTopic = String.format("test-input-topic-%s", UUID.randomUUID().toString());
-    TestUtils.createTopic(zkUtils(), statefulInputKafkaTopic, 32, 1, servers(), new Properties());
+    createTopic(statefulInputKafkaTopic, 32, 1);
 
     // Generate configuration for the test.
     Map<String, String> configMap = buildStreamApplicationConfigMap(ImmutableList.of(statefulInputKafkaTopic), testStreamAppName, testStreamAppId);
@@ -797,7 +776,7 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
     Assert.assertEquals(expectedTaskAssignments, actualTaskAssignments);
 
     // Increase the partition count of the input kafka topic to 64.
-    AdminUtils.addPartitions(zkUtils(), statefulInputKafkaTopic, 64, "", true, RackAwareMode.Enforced$.MODULE$);
+    increasePartitionsTo(statefulInputKafkaTopic, 64);
 
     // Wait for the JobModel version to change due to the increase in the input partition count.
     long jobModelWaitTimeInMillis = 10;
@@ -855,8 +834,8 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
     // Setup the two input kafka topics.
     String statefulInputKafkaTopic1 = String.format("test-input-topic-%s", UUID.randomUUID().toString());
     String statefulInputKafkaTopic2 = String.format("test-input-topic-%s", UUID.randomUUID().toString());
-    TestUtils.createTopic(zkUtils(), statefulInputKafkaTopic1, 32, 1, servers(), new Properties());
-    TestUtils.createTopic(zkUtils(), statefulInputKafkaTopic2, 32, 1, servers(), new Properties());
+    createTopic(statefulInputKafkaTopic1, 32, 1);
+    createTopic(statefulInputKafkaTopic2, 32, 1);
 
     // Generate configuration for the test.
     Map<String, String> configMap = buildStreamApplicationConfigMap(ImmutableList.of(statefulInputKafkaTopic1, statefulInputKafkaTopic2),
@@ -902,10 +881,10 @@ public class TestZkLocalApplicationRunner extends StandaloneIntegrationTestHarne
     Assert.assertEquals(expectedTaskAssignments, actualTaskAssignments);
 
     // Increase the partition count of the input kafka topic1 to 64.
-    AdminUtils.addPartitions(zkUtils(), statefulInputKafkaTopic1, 64, "", true, RackAwareMode.Enforced$.MODULE$);
+    increasePartitionsTo(statefulInputKafkaTopic1, 64);
 
     // Increase the partition count of the input kafka topic2 to 64.
-    AdminUtils.addPartitions(zkUtils(), statefulInputKafkaTopic2, 64, "", true, RackAwareMode.Enforced$.MODULE$);
+    increasePartitionsTo(statefulInputKafkaTopic2, 64);
 
     // Wait for the JobModel version to change due to the increase in the input partition count.
     long jobModelWaitTimeInMillis = 10;

gradle/dependency-versions.gradle
@@ -34,7 +34,7 @@
   jodaTimeVersion = "2.2"
   joptSimpleVersion = "3.2"
   junitVersion = "4.12"
-  kafkaVersion = "0.11.0.2"
+  kafkaVersion = "2.0.1"
   log4jVersion = "1.2.17"
   log4j2Version = "2.11.0"
   metricsVersion = "2.2.0"

samza-kafka/src/main/java/org/apache/samza/system/kafka/KafkaSystemAdmin.java
@@ -23,15 +23,15 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Supplier;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import kafka.utils.ZkUtils;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.admin.AdminClient;
@@ -39,18 +39,22 @@ import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.DeleteTopicsResult;
 import org.apache.kafka.clients.admin.DescribeTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.RecordsToDelete;
 import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.errors.TopicExistsException;
 import org.apache.samza.Partition;
 import org.apache.samza.SamzaException;
+import org.apache.samza.config.ApplicationConfig;
 import org.apache.samza.config.Config;
-import org.apache.samza.config.SystemConfig;
 import org.apache.samza.config.KafkaConfig;
 import org.apache.samza.config.MapConfig;
+import org.apache.samza.config.StreamConfig;
+import org.apache.samza.config.SystemConfig;
 import org.apache.samza.system.StreamSpec;
 import org.apache.samza.system.StreamValidationException;
 import org.apache.samza.system.SystemAdmin;
@@ -58,7 +62,6 @@ import org.apache.samza.system.SystemStream;
 import org.apache.samza.system.SystemStreamMetadata;
 import org.apache.samza.system.SystemStreamPartition;
 import org.apache.samza.util.ExponentialSleepStrategy;
-import org.apache.samza.util.ScalaJavaUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Function0;
@@ -69,8 +72,6 @@ import scala.runtime.AbstractFunction0;
 import scala.runtime.AbstractFunction1;
 import scala.runtime.AbstractFunction2;
 import scala.runtime.BoxedUnit;
-
-import static org.apache.samza.config.KafkaConsumerConfig.*;
 
 
 public class KafkaSystemAdmin implements SystemAdmin {
@@ -91,8 +92,6 @@ public class KafkaSystemAdmin implements SystemAdmin {
   protected final String systemName;
   protected final Consumer metadataConsumer;
   protected final Config config;
-
-  protected kafka.admin.AdminClient adminClientForDelete = null;
 
   // Custom properties to create a new coordinator stream.
   private final Properties coordinatorStreamProperties;
@@ -130,7 +129,7 @@ public class KafkaSystemAdmin implements SystemAdmin {
 
     KafkaConfig kafkaConfig = new KafkaConfig(config);
     coordinatorStreamReplicationFactor = Integer.valueOf(kafkaConfig.getCoordinatorReplicationFactor());
-    coordinatorStreamProperties = KafkaSystemAdminUtilsScala.getCoordinatorTopicProperties(kafkaConfig);
+    coordinatorStreamProperties = getCoordinatorStreamProperties(kafkaConfig);
 
     Map<String, String> storeToChangelog =
         JavaConverters.mapAsJavaMapConverter(kafkaConfig.getKafkaChangelogEnabledStores()).asJava();
@@ -154,9 +153,7 @@ public class KafkaSystemAdmin implements SystemAdmin {
     SystemConfig systemConfig = new SystemConfig(config);
     this.deleteCommittedMessages = systemConfig.deleteCommittedMessages(systemName);
 
-    intermediateStreamProperties =
-        JavaConverters.mapAsJavaMapConverter(KafkaSystemAdminUtilsScala.getIntermediateStreamProperties(config))
-            .asJava();
+    intermediateStreamProperties = getIntermediateStreamProperties(config);
 
     LOG.info(String.format("Created KafkaSystemAdmin for system %s", systemName));
   }
@@ -182,13 +179,6 @@ public class KafkaSystemAdmin implements SystemAdmin {
         metadataConsumer.close();
       } catch (Exception e) {
         LOG.warn("metadataConsumer.close for system " + systemName + " failed with exception.", e);
-      }
-    }
-    if (adminClientForDelete != null) {
-      try {
-        adminClientForDelete.close();
-      } catch (Exception e) {
-        LOG.warn("AdminClient.close() for system {} failed with exception {}.", systemName, e);
       }
     }
 
@@ -451,37 +441,7 @@ public class KafkaSystemAdmin implements SystemAdmin {
       allUpcomingOffsets.putAll(offsetsForTopic.getUpcomingOffsets());
     });
 
-    scala.collection.immutable.Map<String, SystemStreamMetadata> result =
-        KafkaSystemAdminUtilsScala.assembleMetadata(ScalaJavaUtil.toScalaMap(allOldestOffsets),
-            ScalaJavaUtil.toScalaMap(allNewestOffsets), ScalaJavaUtil.toScalaMap(allUpcomingOffsets));
-
-    LOG.debug("assembled SystemStreamMetadata is: {}", result);
-    return JavaConverters.mapAsJavaMapConverter(result).asJava();
-  }
-
-  private String fetchNewestOffset(SystemStreamPartition ssp) {
-    LOG.debug("Fetching newest offset for {}", ssp);
-    String newestOffset;
-
-    TopicPartition topicPartition = new TopicPartition(ssp.getStream(), ssp.getPartition().getPartitionId());
-
-    // the offsets returned from the consumer is the Long type
-    Long upcomingOffset =
-        (Long) metadataConsumer.endOffsets(Collections.singletonList(topicPartition)).get(topicPartition);
-
-    // Kafka's "latest" offset is always last message in stream's offset + 1,
-    // so get newest message in stream by subtracting one. This is safe
-    // even for key-deduplicated streams, since the last message will
-    // never be deduplicated.
-    if (upcomingOffset <= 0) {
-      LOG.debug("Stripping newest offsets for {} because the topic appears empty.", topicPartition);
-      newestOffset = null;
-    } else {
-      newestOffset = String.valueOf(upcomingOffset - 1);
-    }
-
-    LOG.info("Newest offset for ssp {} is: {}", ssp, newestOffset);
-    return newestOffset;
+    return assembleMetadata(allOldestOffsets, allNewestOffsets, allUpcomingOffsets);
   }
 
   @Override
@@ -569,7 +529,7 @@ public class KafkaSystemAdmin implements SystemAdmin {
       }
 
       kafkaSpec = new KafkaStreamSpec(spec.getId(), topicName, systemName, spec.getPartitionCount(),
-          topicMeta.replicationFactor(), topicMeta.kafkaProps());
+          topicMeta.getReplicationFactor(), topicMeta.getKafkaProperties());
     } else if (spec.isCoordinatorStream()) {
       kafkaSpec =
           new KafkaStreamSpec(spec.getId(), spec.getPhysicalName(), systemName, 1, coordinatorStreamReplicationFactor,
@@ -628,10 +588,18 @@ public class KafkaSystemAdmin implements SystemAdmin {
   @Override
   public void deleteMessages(Map<SystemStreamPartition, String> offsets) {
     if (deleteCommittedMessages) {
-      if (adminClientForDelete == null) {
-        adminClientForDelete = kafka.admin.AdminClient.create(createAdminClientProperties());
-      }
-      KafkaSystemAdminUtilsScala.deleteMessages(adminClientForDelete, offsets);
+      Map<TopicPartition, RecordsToDelete> recordsToDelete = offsets.entrySet()
+          .stream()
+          .collect(Collectors.toMap(entry ->
+              new TopicPartition(entry.getKey().getStream(), entry.getKey().getPartition().getPartitionId()),
+              entry -> RecordsToDelete.beforeOffset(Long.parseLong(entry.getValue()) + 1)));
+
+      adminClient.deleteRecords(recordsToDelete).all().whenComplete((ignored, exception) -> {
+        if (exception != null) {
+          LOG.error("Delete message failed for SSPs " + offsets.keySet() + " due to", exception);
+        }
+      });
+
       deleteMessageCalled = true;
     }
   }
@@ -656,25 +624,7 @@ public class KafkaSystemAdmin implements SystemAdmin {
     }
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
 
-    // kafka.admin.AdminUtils requires zkConnect
-    // this will change after we move to the new org.apache..AdminClient
-    String zkConnect =
-        config.get(String.format(KafkaConfig.CONSUMER_CONFIGS_CONFIG_KEY(), systemName, ZOOKEEPER_CONNECT));
-    if (StringUtils.isBlank(zkConnect)) {
-      throw new SamzaException("Missing zookeeper.connect config for admin for system " + systemName);
-    }
-    props.put(ZOOKEEPER_CONNECT, zkConnect);
-
     return props;
-  }
-
-  private Supplier<ZkUtils> getZkConnection() {
-    String zkConnect =
-        config.get(String.format(KafkaConfig.CONSUMER_CONFIGS_CONFIG_KEY(), systemName, ZOOKEEPER_CONNECT));
-    if (StringUtils.isBlank(zkConnect)) {
-      throw new SamzaException("Missing zookeeper.connect config for admin for system " + systemName);
-    }
-    return () -> ZkUtils.apply(zkConnect, 6000, 6000, false);
   }
 
   @Override
@@ -682,6 +632,79 @@ public class KafkaSystemAdmin implements SystemAdmin {
     return ((Set<String>) this.metadataConsumer.listTopics().keySet()).stream()
         .map(x -> new SystemStream(systemName, x))
         .collect(Collectors.toSet());
+  }
+
+  /**
+   * A helper method that takes oldest, newest, and upcoming offsets for each
+   * system stream partition, and creates a single map from stream name to
+   * SystemStreamMetadata.
+   *
+   * @param newestOffsets map of SSP to newest offset
+   * @param oldestOffsets map of SSP to oldest offset
+   * @param upcomingOffsets map of SSP to upcoming offset
+   * @return a {@link Map} from {@code system} to {@link SystemStreamMetadata}
+   */
+  @VisibleForTesting
+  static Map<String, SystemStreamMetadata> assembleMetadata(Map<SystemStreamPartition, String> oldestOffsets,
+      Map<SystemStreamPartition, String> newestOffsets, Map<SystemStreamPartition, String> upcomingOffsets) {
+    HashSet<SystemStreamPartition> allSSPs = new HashSet<>();
+    allSSPs.addAll(oldestOffsets.keySet());
+    allSSPs.addAll(newestOffsets.keySet());
+    allSSPs.addAll(upcomingOffsets.keySet());
+
+    Map<String, SystemStreamMetadata> assembledMetadata = allSSPs.stream()
+        .collect(Collectors.groupingBy(SystemStreamPartition::getStream))
+        .entrySet()
+        .stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+          Map<Partition, SystemStreamMetadata.SystemStreamPartitionMetadata> partitionMetadata =
+              entry.getValue()
+                  .stream()
+                  .collect(Collectors.toMap(SystemStreamPartition::getPartition, ssp ->
+                      new SystemStreamMetadata.SystemStreamPartitionMetadata(
+                          oldestOffsets.getOrDefault(ssp, null),
+                          newestOffsets.getOrDefault(ssp, null),
+                          upcomingOffsets.get(ssp))));
+          return new SystemStreamMetadata(entry.getKey(), partitionMetadata);
+        }));
+
+    return assembledMetadata;
+  }
+
+  /**
+   * Fetch stream properties for all intermediate streams.
+   *
+   * @param config kafka system config
+   * @return a {@link Map} from {@code streamId} to stream {@link Properties}
+   */
+  @VisibleForTesting
+  static Map<String, Properties> getIntermediateStreamProperties(Config config) {
+    Map<String, Properties> intermedidateStreamProperties = Collections.emptyMap();
+    ApplicationConfig appConfig = new ApplicationConfig(config);
+
+    if (appConfig.getAppMode() == ApplicationConfig.ApplicationMode.BATCH) {
+      StreamConfig streamConfig = new StreamConfig(config);
+      intermedidateStreamProperties = JavaConverters.asJavaCollectionConverter(streamConfig.getStreamIds())
+          .asJavaCollection()
+          .stream()
+          .filter(streamConfig::getIsIntermediateStream)
+          .collect(Collectors.toMap(Function.identity(), streamId -> {
+            Properties properties = new Properties();
+            properties.putAll(streamConfig.getStreamProperties(streamId));
+            properties.putIfAbsent(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(KafkaConfig.DEFAULT_RETENTION_MS_FOR_BATCH()));
+            return properties;
+          }));
+    }
+
+    return intermedidateStreamProperties;
+  }
+
+  private Properties getCoordinatorStreamProperties(KafkaConfig config) {
+    Properties coordinatorStreamProperties = new Properties();
+    coordinatorStreamProperties.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT);
+    coordinatorStreamProperties.put(TopicConfig.SEGMENT_BYTES_CONFIG, config.getCoordinatorSegmentBytes());
+
+    return coordinatorStreamProperties;
   }
 
   /**
@@ -709,6 +732,31 @@ public class KafkaSystemAdmin implements SystemAdmin {
 
     private Map<SystemStreamPartition, String> getUpcomingOffsets() {
       return upcomingOffsets;
+    }
+  }
+
+  /**
+   * A helper class for represent changelog related information.
+   */
+  private static class ChangelogInfo {
+    final int replicationFactor;
+    final Properties kafkaProperties;
+
+    /**
+     * @param replicationFactor The number of replicas for the changelog stream
+     * @param kafkaProperties The kafka specific properties that need to be used for changelog stream creation
+     */
+    ChangelogInfo(int replicationFactor, Properties kafkaProperties) {
+      this.replicationFactor = replicationFactor;
+      this.kafkaProperties = kafkaProperties;
+    }
+
+    public int getReplicationFactor() {
+      return replicationFactor;
+    }
+
+    public Properties getKafkaProperties() {
+      return kafkaProperties;
     }
   }
 }

samza-kafka/src/main/scala/org/apache/samza/config/KafkaSerdeConfig.scala
@@ -1,48 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-package org.apache.samza.config
-
-import java.util.Properties
-
-import kafka.utils.VerifiableProperties
-
-object KafkaSerdeConfig {
-  // kafka serde config constants
-  val ENCODER = SerializerConfig.SERIALIZER_PREFIX + ".encoder"
-  val DECODER = SerializerConfig.SERIALIZER_PREFIX + ".decoder"
-
-  implicit def Config2KafkaSerde(config: Config) = new KafkaSerdeConfig(config)
-}
-
-class KafkaSerdeConfig(config: Config) extends ScalaMapConfig(config) {
-  def getKafkaEncoder(serializer: String) =
-    getOption(KafkaSerdeConfig.ENCODER format serializer)
-
-  def getKafkaDecoder(serializer: String) =
-    getOption(KafkaSerdeConfig.DECODER format serializer)
-
-  def getKafkaProperties(serializer: String) = {
-    val properties = new Properties();
-    val prefix = SerializerConfig.SERIALIZER_PREFIX format serializer + "."
-    properties.putAll(config.subset(prefix, true))
-
-    new VerifiableProperties(properties);
-  }
-}

commit e749af78d3b89b137ba94d28f15dfc8e0e558d58

Fix ZkLocalApplicationRunner regression introduced by PR #951

samza-test/src/test/java/org/apache/samza/test/processor/TestZkLocalApplicationRunner.java
@@ -153,10 +153,16 @@ public class TestZkLocalApplicationRunner extends IntegrationTestHarness {
     zkUtils = new ZkUtils(zkKeyBuilder, zkClient, ZK_CONNECTION_TIMEOUT_MS, ZK_SESSION_TIMEOUT_MS, new NoOpMetricsRegistry());
     zkUtils.connect();
 
+    ImmutableMap<String, Integer> topicToPartitionCount = ImmutableMap.of(
+        inputSinglePartitionKafkaTopic, 1,
+        outputSinglePartitionKafkaTopic, 1,
+        inputKafkaTopic, 5,
+        outputKafkaTopic, 5);
+
     List<NewTopic> newTopics =
         ImmutableList.of(inputKafkaTopic, outputKafkaTopic, inputSinglePartitionKafkaTopic, outputSinglePartitionKafkaTopic)
             .stream()
-            .map(topic -> new NewTopic(topic, 5, (short) 1))
+            .map(topic -> new NewTopic(topic, topicToPartitionCount.get(topic), (short) 1))
             .collect(Collectors.toList());
 
     assertTrue("Encountered errors during test setup. Failed to create topics.", createTopics(newTopics));