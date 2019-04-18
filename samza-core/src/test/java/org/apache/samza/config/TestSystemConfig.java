ca8bb23a8b68cdbd627a7e63714e37cbe5de3804
Shanthoosh Venkataraman <spvenkat@usc.edu>
2019-04-18 12:33:54 -0700

    Fix checkstyle errors.

--- samza-core/src/test/java/org/apache/samza/config/TestSystemConfig.java
+++ samza-core/src/test/java/org/apache/samza/config/TestSystemConfig.java
@@ -31,7 +31,6 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
