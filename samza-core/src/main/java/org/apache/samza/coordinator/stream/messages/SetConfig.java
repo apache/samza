bb5e395265ac78be713b06c18e4132fdb73ac259
Shanthoosh Venkataraman <spvenkat@usc.edu>
2019-04-24 09:17:55 -0700

    Move the isDelete check to the CoordinatorStreamMessage.

--- samza-core/src/main/java/org/apache/samza/coordinator/stream/messages/SetConfig.java
+++ samza-core/src/main/java/org/apache/samza/coordinator/stream/messages/SetConfig.java
@@ -54,13 +54,6 @@ public class SetConfig extends CoordinatorStreamMessage {
    * @return the configuration as a string
    */
   public String getConfigValue() {
-    // Unlike other CoordinatorStreamMessage, SetConfig message(configurations) are deleted with a seperate DELETE CoordinatorStreamMessage.
-    // For deleted configuration messages, the message value map in DELETE message is null. This isDelete check saves us
-    // from dereferencing a null map.
-    if (isDelete()) {
-      return null;
-    } else {
-      return getMessageValue(CONFIG_VALUE_KEY);
-    }
+    return getMessageValue(CONFIG_VALUE_KEY);
   }
 }
