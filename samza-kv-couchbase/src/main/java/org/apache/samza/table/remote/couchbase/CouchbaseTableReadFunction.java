e874072fb25bcb98d36f475bf996539284679b1f
Yixing Zhang <tommykuaidlaa@gmail.com>
2019-04-25 18:33:45 -0700

    SAMZA-2156: Couchbase support for Samza Table API
    Implemented CouchbaseTableReadFunction
    Implemented CouchbaseTableWriteFunction
    Tested with unit tests and integration test
    
    Author: Yixing Zhang <tommykuaidlaa@gmail.com>
    
    Reviewers: Wei Song <wsong@linkedin.com>
    
    Closes #986 from Zhangyx39/couchbase

---
+++ samza-kv-couchbase/src/main/java/org/apache/samza/table/remote/couchbase/CouchbaseTableReadFunction.java
@@ -0,0 +1,147 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.table.remote.couchbase;
+
+import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
+import com.couchbase.client.deps.io.netty.util.ReferenceCountUtil;
+import com.couchbase.client.java.document.BinaryDocument;
+import com.couchbase.client.java.document.Document;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.google.common.base.Preconditions;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.samza.SamzaException;
+import org.apache.samza.context.Context;
+import org.apache.samza.table.remote.TableReadFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.Single;
+import rx.SingleSubscriber;
+
+
+/**
+ * TableReadFunction implementation for reading from Couchbase. The value type can be either {@link JsonObject} or
+ * any other Object. If the value type is JsonObject, data in JSON format will be read from Couchbase directly as
+ * JsonObject. Otherwise, a {@link org.apache.samza.serializers.Serde} needs to be provided to serialize and
+ * deserialize the value object.
+ * @param <V> Type of values to read from Couchbase
+ */
+public class CouchbaseTableReadFunction<V> extends BaseCouchbaseTableFunction<V>
+    implements TableReadFunction<String, V> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CouchbaseTableReadFunction.class);
+
+  protected final Class<? extends Document<?>> documentType;
+
+  /**
+   * Construct an instance of {@link CouchbaseTableReadFunction}.
+   * @param bucketName Name of the couchbase bucket
+   * @param clusterNodes Some Hosts of the Couchbase cluster. Recommended to provide more than one nodes so that if
+   *                     the first node could not be connected, other nodes can be tried.
+   * @param valueClass Type of values
+   */
+  public CouchbaseTableReadFunction(String bucketName, Class<V> valueClass, String... clusterNodes) {
+    super(bucketName, valueClass, clusterNodes);
+    documentType = JsonObject.class.isAssignableFrom(valueClass) ? JsonDocument.class : BinaryDocument.class;
+  }
+
+  @Override
+  public void init(Context context) {
+    super.init(context);
+    LOGGER.info("Read function for bucket {} initialized successfully", bucketName);
+  }
+
+  @Override
+  public CompletableFuture<V> getAsync(String key) {
+    Preconditions.checkArgument(StringUtils.isNotBlank(key), "key must not be null, empty or blank");
+    CompletableFuture<V> future = new CompletableFuture<>();
+    Single<? extends Document<?>> singleObservable =
+        bucket.async().get(key, documentType, timeout.toMillis(), TimeUnit.MILLISECONDS).toSingle();
+    singleObservable.subscribe(new SingleSubscriber<Document<?>>() {
+      @Override
+      public void onSuccess(Document<?> document) {
+        if (document != null) {
+          if (document instanceof BinaryDocument) {
+            handleGetAsyncBinaryDocument((BinaryDocument) document, future, key);
+          } else {
+            // V is of type JsonObject
+            future.complete((V) document.content());
+          }
+        } else {
+          // The Couchbase async client should not return null
+          future.completeExceptionally(new SamzaException(String.format("Got unexpected null value from key %s", key)));
+        }
+      }
+
+      @Override
+      public void onError(Throwable throwable) {
+        if (throwable instanceof NoSuchElementException) {
+          // There is no element returned by the observable, meaning the key doesn't exist.
+          future.complete(null);
+        } else {
+          future.completeExceptionally(new SamzaException(String.format("Failed to get key %s", key), throwable));
+        }
+      }
+    });
+    return future;
+  }
+
+  /**
+   * Helper method to read bytes from binaryDocument and release the buffer.
+   */
+  private void handleGetAsyncBinaryDocument(BinaryDocument binaryDocument, CompletableFuture<V> future, String key) {
+    ByteBuf buffer = binaryDocument.content();
+    try {
+      byte[] bytes;
+      if (buffer.hasArray() && buffer.arrayOffset() == 0 && buffer.readableBytes() == buffer.array().length) {
+        bytes = buffer.array();
+      } else {
+        bytes = new byte[buffer.readableBytes()];
+        buffer.readBytes(bytes);
+      }
+      future.complete(valueSerde.fromBytes(bytes));
+    } catch (Exception e) {
+      future.completeExceptionally(
+          new SamzaException(String.format("Failed to deserialize value of key %s with given serde", key), e));
+    } finally {
+      ReferenceCountUtil.release(buffer);
+    }
+  }
+}
---
+++ samza-kv-couchbase/src/test/java/org/apache/samza/table/remote/couchbase/TestCouchbaseTableWriteFunction.java
@@ -0,0 +1,198 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.samza.table.remote.couchbase;
+
+import com.couchbase.client.core.CouchbaseException;
+import com.couchbase.client.java.AsyncBucket;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.Document;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.error.TemporaryFailureException;
+import com.couchbase.client.java.error.TemporaryLockFailureException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.apache.samza.SamzaException;
+import org.apache.samza.serializers.Serde;
+import org.apache.samza.serializers.StringSerde;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import rx.Observable;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+
+/**
+ * This class performs unit tests for CouchbaseTableWriteFunction.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CouchbaseBucketRegistry.class)
+public class TestCouchbaseTableWriteFunction {
+  private static final String DEFAULT_BUCKET_NAME = "default-bucket-name";
+  private static final String DEFAULT_CLUSTER_NODE = "localhost";
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidBucketName() {
+    new CouchbaseTableWriteFunction<>("", String.class, DEFAULT_CLUSTER_NODE);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidClusterNodes() {
+    new CouchbaseTableWriteFunction<>(DEFAULT_BUCKET_NAME, String.class);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidValueClass() {
+    new CouchbaseTableWriteFunction<>(DEFAULT_BUCKET_NAME, null, DEFAULT_CLUSTER_NODE);
+  }
+
+  @Test
+  public void testIsNotRetriable() {
+    CouchbaseTableWriteFunction writeFunction = createAndInit();
+    assertFalse(writeFunction.isRetriable(null));
+    assertFalse(writeFunction.isRetriable(new SamzaException()));
+    assertFalse(writeFunction.isRetriable(new SamzaException("", new IllegalArgumentException())));
+  }
+
+  @Test
+  public void testIsRetriable() {
+    CouchbaseTableWriteFunction writeFunction = createAndInit();
+    assertTrue(writeFunction.isRetriable(new TemporaryFailureException()));
+    assertTrue(writeFunction.isRetriable(new TemporaryLockFailureException()));
+    assertTrue(writeFunction.isRetriable(new SamzaException(new TemporaryLockFailureException())));
+    assertTrue(writeFunction.isRetriable(new SamzaException(new TemporaryFailureException())));
+    assertTrue(writeFunction.isRetriable(
+        new RuntimeException(new SamzaException(new RuntimeException(new TemporaryFailureException())))));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testPutAsyncNullKey() {
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit();
+    writeFunction.putAsync(null, JsonObject.create());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testPutAsyncKeyWithSpaces() {
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit();
+    writeFunction.putAsync("k e y", JsonObject.create());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testPutAsyncNullValue() {
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit();
+    writeFunction.putAsync("key", null);
+  }
+
+  @Test
+  public void testPutAsyncException() {
+    String key = "throwExceptionKey";
+    JsonObject value = JsonObject.create();
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit(bucket, asyncBucket);
+    when(asyncBucket.upsert(any(Document.class), anyLong(), any(TimeUnit.class))).thenReturn(
+        Observable.error(new CouchbaseException()));
+    assertTrue(writeFunction.putAsync(key, value).isCompletedExceptionally());
+  }
+
+  @Test
+  public void testPutAsyncJsonObjectValue() throws Exception {
+    String key = "key";
+    JsonObject value = JsonObject.fromJson("{\"id\": 1}");
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit(bucket, asyncBucket);
+    when(asyncBucket.upsert(any(Document.class), anyLong(), any(TimeUnit.class))).thenReturn(Observable.just(null));
+    assertNull(writeFunction.putAsync(key, value).get());
+  }
+
+  @Test
+  public void testPutAsyncStringValue() throws Exception {
+    String key = "key";
+    String value = "value";
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<String> writeFunction =
+        createAndInit(String.class, new StringSerde(), bucket, asyncBucket);
+    when(asyncBucket.upsert(any(Document.class), anyLong(), any(TimeUnit.class))).thenReturn(Observable.just(null));
+    assertNull(writeFunction.putAsync(key, value).get());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDeleteAsyncNullKey() {
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit();
+    writeFunction.deleteAsync(null);
+  }
+
+  @Test
+  public void testDeleteAsyncException() {
+    String key = "throwExceptionKey";
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit(bucket, asyncBucket);
+    when(asyncBucket.remove(eq(key), anyLong(), any(TimeUnit.class))).thenReturn(
+        Observable.error(new CouchbaseException()));
+    assertTrue(writeFunction.deleteAsync(key).isCompletedExceptionally());
+  }
+
+  @Test
+  public void testDeleteAsyncJsonObjectValue() throws Exception {
+    String key = "key";
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<JsonObject> writeFunction = createAndInit(bucket, asyncBucket);
+    when(asyncBucket.remove(eq(key), anyLong(), any(TimeUnit.class))).thenReturn(Observable.just(null));
+    assertNull(writeFunction.deleteAsync(key).get());
+  }
+
+  @Test
+  public void testDeleteAsyncStringValue() throws Exception {
+    String key = "key";
+    Bucket bucket = mock(Bucket.class);
+    AsyncBucket asyncBucket = mock(AsyncBucket.class);
+    CouchbaseTableWriteFunction<String> writeFunction =
+        createAndInit(String.class, new StringSerde(), bucket, asyncBucket);
+    when(asyncBucket.remove(eq(key), anyLong(), any(TimeUnit.class))).thenReturn(Observable.just(null));
+    assertNull(writeFunction.deleteAsync(key).get());
+  }
+
+  private CouchbaseTableWriteFunction<JsonObject> createAndInit() {
+    return createAndInit(mock(Bucket.class), mock(AsyncBucket.class));
+  }
+
+  private CouchbaseTableWriteFunction<JsonObject> createAndInit(Bucket bucket, AsyncBucket asyncBucket) {
+    return createAndInit(JsonObject.class, null, bucket, asyncBucket);
+  }
+
+  private <V> CouchbaseTableWriteFunction<V> createAndInit(Class<V> valueClass, Serde<V> serde, Bucket bucket,
+      AsyncBucket asyncBucket) {
+    when(bucket.async()).thenReturn(asyncBucket);
+    PowerMockito.stub(PowerMockito.method(CouchbaseBucketRegistry.class, "getBucket", String.class, List.class,
+        CouchbaseEnvironmentConfigs.class)).toReturn(bucket);
+    CouchbaseTableWriteFunction<V> readFunction =
+        new CouchbaseTableWriteFunction<>(DEFAULT_BUCKET_NAME, valueClass, DEFAULT_CLUSTER_NODE).withSerde(serde);
+    readFunction.init(null);
+    return readFunction;
+  }
+}
