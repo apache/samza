1772177bf4e3c93308028fcf3f3f821c9d4a6ea6
Wei Song <wsong@linkedin.com>
2019-05-20 09:49:32 -0700

    SAMZA-2200: Update table sendTo() and join() operation to accept additional arguments (#1041)

--- samza-api/src/main/java/org/apache/samza/table/Table.java
+++ samza-api/src/main/java/org/apache/samza/table/Table.java
@@ -34,11 +34,11 @@ import org.apache.samza.task.InitableTask;
  * A {@link Table} is an abstraction for data sources that support random access by key. It is an evolution of the
  * existing {@link KeyValueStore} API. It offers support for both local and remote data sources and composition through
  * hybrid tables. For remote data sources, a {@code RemoteTable} provides optimized access with caching, rate-limiting,
- * and retry support.
+ * batching, and retry support.
  * <p>
  * Use a {@link TableDescriptor} to specify the properties of a {@link Table}. For High Level API
  * {@link StreamApplication}s, use {@link StreamApplicationDescriptor#getTable} to obtain the {@link Table} instance for
- * the descriptor that can be used with the {@link MessageStream} operators like {@link MessageStream#sendTo(Table)}.
+ * the descriptor that can be used with the {@link MessageStream} operators like {@link MessageStream#sendTo(Table, Object[])}.
  * Alternatively, use {@link TaskContext#getTable(String)} in {@link InitableFunction#init} to get the table instance
  * for use within operator functions. For Low Level API {@link TaskApplication}s, use {@link TaskContext#getTable}
  * in {@link InitableTask#init} to get the table instance for use within the Task.
