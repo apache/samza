5f8b6d47affbf1c8a101ec0f8c70e8641437c805
Daniel Nishimura <dnishimura@gmail.com>
2019-05-23 13:35:52 -0700

    SAMZA-2201 : Startpoints - Integrate fan out with job coordinators (#1042)
    * SAMZA-2201 : Startpoints - Integrate fan out with job coordinators
    
    * Address PR comments from @cameronlee314
    
    * Remove extra whitespaces.
    
    * Address latest comments.

---
+++ samza-core/src/main/java/org/apache/samza/job/model/JobModelUtil.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.samza.job.model;
+
+import com.google.common.base.Preconditions;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.apache.samza.container.TaskName;
+import org.apache.samza.system.SystemStreamPartition;
+
+/**
+ * Utility class for the {@link JobModel}
+ */
+public class JobModelUtil {
+
+  /**
+   * Extracts the map of {@link SystemStreamPartition}s to {@link TaskName} from the {@link JobModel}
+   *
+   * @return the extracted map
+   */
+  public static Map<TaskName, Set<SystemStreamPartition>> getTaskToSystemStreamPartitions(JobModel jobModel) {
+    Preconditions.checkArgument(jobModel != null, "JobModel cannot be null");
+
+    Map<String, ContainerModel> containers = jobModel.getContainers();
+    HashMap<TaskName, Set<SystemStreamPartition>> taskToSSPs = new HashMap<>();
+    for (ContainerModel containerModel : containers.values()) {
+      for (TaskName taskName : containerModel.getTasks().keySet()) {
+        TaskModel taskModel = containerModel.getTasks().get(taskName);
+        if (taskModel.getTaskMode() != TaskMode.Active) {
+          // Avoid duplicate tasks
+          continue;
+        }
+        taskToSSPs.putIfAbsent(taskName, new HashSet<>());
+        taskToSSPs.get(taskName).addAll(taskModel.getSystemStreamPartitions());
+      }
+    }
+    return taskToSSPs;
+  }
+
+  private JobModelUtil() { }
+}
