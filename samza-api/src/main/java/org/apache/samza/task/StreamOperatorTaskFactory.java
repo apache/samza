254d2718e760704484338492fd13ebb628f91e1d
mynameborat <bharath.kumarasubramanian@gmail.com>
2019-06-03 08:49:05 -0700

    SAMZA-2232: Move StreamOperatorTaskFactory to samza-api

--- samza-api/src/main/java/org/apache/samza/task/StreamOperatorTaskFactory.java
+++ samza-api/src/main/java/org/apache/samza/task/StreamOperatorTaskFactory.java
@@ -20,11 +20,11 @@ package org.apache.samza.task;
 
 
 /**
- * Build {@link StreamOperatorTask} instances.
+ * Build {@link AsyncStreamTask} instances.
  * <p>
- * Implementations should return a new instance of {@link StreamOperatorTask} for each {@link #createInstance()} invocation.
+ * Implementations should return a new instance of {@link AsyncStreamTask} for each {@link #createInstance()} invocation.
  * Note: It is not part of samza-api as it is a temporary hack introduced for SAMZA-2172. It will eventually
  * go away with SAMZA-2203
  */
-interface StreamOperatorTaskFactory extends TaskFactory<AsyncStreamTask> {
+public interface StreamOperatorTaskFactory extends TaskFactory<AsyncStreamTask> {
 }
