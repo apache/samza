9ade79e2263d1a44bfed7acc37aaf4cf580671df
Cameron Lee <calee@linkedin.com>
2019-11-15 19:53:54 -0800

    fix license and build issues

--- samza-core/src/main/java/org/apache/samza/util/Util.java
+++ samza-core/src/main/java/org/apache/samza/util/Util.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.samza.util;
 
 import java.net.Inet4Address;
@@ -14,8 +32,6 @@ import com.google.common.collect.Lists;
 import org.apache.samza.SamzaException;
 import org.apache.samza.config.ApplicationConfig;
 import org.apache.samza.config.Config;
-import org.apache.samza.config.ConfigRewriter;
-import org.apache.samza.config.JobConfig;
 import org.apache.samza.config.TaskConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,9 +51,9 @@ public class Util {
 
   public static String getSamzaVersion() {
     return Optional.ofNullable(Util.class.getPackage().getImplementationVersion()).orElseGet(() -> {
-      LOG.warn("Unable to find implementation samza version in jar's meta info. Defaulting to {}", FALLBACK_VERSION);
-      return FALLBACK_VERSION;
-    });
+        LOG.warn("Unable to find implementation samza version in jar's meta info. Defaulting to {}", FALLBACK_VERSION);
+        return FALLBACK_VERSION;
+      });
   }
 
   public static String getTaskClassVersion(Config config) {
