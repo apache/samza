f64d6cfe529c9c9a7f33662a8241e0849791b04d
Ray Matharu <rmatharu@linkedin.com>
2019-11-21 21:58:23 -0800

    Adding unit test for metrics

--- samza-kv-rocksdb/src/test/scala/org/apache/samza/storage/kv/TestRocksDbKeyValueStore.scala
+++ samza-kv-rocksdb/src/test/scala/org/apache/samza/storage/kv/TestRocksDbKeyValueStore.scala
@@ -210,4 +210,27 @@ class TestRocksDbKeyValueStore
 
     rocksDB.close()
   }
+
+  @Test
+  def testRocksDBMetricsWithBulkLoadRWRecreate(): Unit = {
+    val registry = new MetricsRegistryMap("registrymap")
+    val metrics = new KeyValueStoreMetrics("dbstore", registry)
+
+    // Sample metric values for estimate-num-keys metrics
+    val bulkloadStoreMetricValue = "100"
+    val readWriteStoreMetricValue = "10"
+
+    // Metric during bulk-load/bootstrap
+    metrics.newGauge("estimate-num-keys", () => bulkloadStoreMetricValue)
+
+    assert(registry.getGroup("org.apache.samza.storage.kv.KeyValueStoreMetrics").
+      get("dbstore-estimate-num-keys").asInstanceOf[Gauge[String]].getValue.eq("100"))
+
+    // Bulk-load complete, new store in read-write mode
+    metrics.newGauge("estimate-num-keys", () => readWriteStoreMetricValue)
+
+    assert(registry.getGroup("org.apache.samza.storage.kv.KeyValueStoreMetrics").
+      get("dbstore-estimate-num-keys").asInstanceOf[Gauge[String]].getValue.eq("10"))
+  }
+
 }
