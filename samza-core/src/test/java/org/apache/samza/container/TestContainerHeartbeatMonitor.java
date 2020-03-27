43eb82acf9cc576cf5b9771cfe70e29c4c74bcf5
Cameron Lee <calee@linkedin.com>
2020-03-27 14:39:17 -0700

    refactor test to prevent waiting

--- samza-core/src/test/java/org/apache/samza/container/TestContainerHeartbeatMonitor.java
+++ samza-core/src/test/java/org/apache/samza/container/TestContainerHeartbeatMonitor.java
@@ -22,10 +22,11 @@ package org.apache.samza.container;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
@@ -37,57 +38,78 @@ import static org.mockito.Mockito.when;
 
 
 public class TestContainerHeartbeatMonitor {
+  @Mock
+  private Runnable onExpired;
+  @Mock
+  private ContainerHeartbeatClient containerHeartbeatClient;
+
+  private ScheduledExecutorService scheduler;
+  /**
+   * Use this to detect when the scheduler has finished executing the fixed-rate task.
+   */
+  private CountDownLatch schedulerFixedRateExecutionLatch;
+
+  private ContainerHeartbeatMonitor containerHeartbeatMonitor;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    this.schedulerFixedRateExecutionLatch = new CountDownLatch(1);
+    this.scheduler = buildScheduledExecutorService(this.schedulerFixedRateExecutionLatch);
+    this.containerHeartbeatMonitor =
+        new ContainerHeartbeatMonitor(this.onExpired, this.containerHeartbeatClient, this.scheduler);
+  }
+
   @Test
   public void testCallbackWhenHeartbeatDead() throws InterruptedException {
-    ContainerHeartbeatClient mockClient = mock(ContainerHeartbeatClient.class);
-    CountDownLatch countDownLatch = new CountDownLatch(1);
-    Runnable onExpired = countDownLatch::countDown;
     ContainerHeartbeatResponse response = new ContainerHeartbeatResponse(false);
-    when(mockClient.requestHeartbeat()).thenReturn(response);
-    ScheduledExecutorService scheduler = buildScheduledExecutorService();
-    ContainerHeartbeatMonitor monitor = new ContainerHeartbeatMonitor(onExpired, mockClient, scheduler);
-    monitor.start();
-    boolean success = countDownLatch.await(2, TimeUnit.SECONDS);
-    assertTrue(success);
+    when(this.containerHeartbeatClient.requestHeartbeat()).thenReturn(response);
+    this.containerHeartbeatMonitor.start();
+    // wait for the executor to finish the heartbeat check task
+    boolean fixedRateTaskCompleted = this.schedulerFixedRateExecutionLatch.await(2, TimeUnit.SECONDS);
+    assertTrue("Did not complete heartbeat check", fixedRateTaskCompleted);
     // check that the shutdown task got submitted, but don't actually execute it since it will shut down the process
-    verify(scheduler).schedule(any(Runnable.class), eq((long) ContainerHeartbeatMonitor.SHUTDOWN_TIMOUT_MS),
+    verify(this.scheduler).schedule(any(Runnable.class), eq((long) ContainerHeartbeatMonitor.SHUTDOWN_TIMOUT_MS),
         eq(TimeUnit.MILLISECONDS));
+    verify(this.onExpired).run();
 
-    monitor.stop();
-    verify(scheduler).shutdown();
+    this.containerHeartbeatMonitor.stop();
+    verify(this.scheduler).shutdown();
   }
 
   @Test
   public void testDoesNotCallbackWhenHeartbeatAlive() throws InterruptedException {
-    ContainerHeartbeatClient client = mock(ContainerHeartbeatClient.class);
-    CountDownLatch countDownLatch = new CountDownLatch(1);
-    Runnable onExpired = countDownLatch::countDown;
     ContainerHeartbeatResponse response = new ContainerHeartbeatResponse(true);
-    when(client.requestHeartbeat()).thenReturn(response);
-    ScheduledExecutorService scheduler = buildScheduledExecutorService();
-    ContainerHeartbeatMonitor monitor = new ContainerHeartbeatMonitor(onExpired, client, scheduler);
-    monitor.start();
-    boolean success = countDownLatch.await(2, TimeUnit.SECONDS);
-    assertFalse(success);
-    assertEquals(1, countDownLatch.getCount());
+    when(this.containerHeartbeatClient.requestHeartbeat()).thenReturn(response);
+    this.containerHeartbeatMonitor.start();
+    // wait for the executor to finish the heartbeat check task
+    boolean fixedRateTaskCompleted = this.schedulerFixedRateExecutionLatch.await(2, TimeUnit.SECONDS);
+    assertTrue("Did not complete heartbeat check", fixedRateTaskCompleted);
     // shutdown task should not have been submitted
-    verify(scheduler, never()).schedule(any(Runnable.class), anyLong(), any());
+    verify(this.scheduler, never()).schedule(any(Runnable.class), anyLong(), any());
+    verify(this.onExpired, never()).run();
 
-    monitor.stop();
-    verify(scheduler).shutdown();
+    this.containerHeartbeatMonitor.stop();
+    verify(this.scheduler).shutdown();
   }
 
   /**
-   * Build a mock {@link ScheduledExecutorService} which will execute a fixed-rate task once. It will not execute any
-   * one-shot tasks, but it can be used to verify that the one-shot task was submitted.
+   * Build a mock {@link ScheduledExecutorService} which will execute a fixed-rate task once. It will count down on
+   * {@code schedulerFixedRateExecutionLatch} when the task is finished executing.
+   * It will not execute any one-shot tasks, but it can be used to verify that the one-shot task was submitted.
    */
-  private static ScheduledExecutorService buildScheduledExecutorService() {
+  private static ScheduledExecutorService buildScheduledExecutorService(
+      CountDownLatch schedulerFixedRateExecutionLatch) {
     ScheduledExecutorService scheduler = mock(ScheduledExecutorService.class);
     when(scheduler.scheduleAtFixedRate(any(), eq(0L), eq((long) ContainerHeartbeatMonitor.SCHEDULE_MS),
         eq(TimeUnit.MILLISECONDS))).thenAnswer(invocation -> {
             Runnable command = invocation.getArgumentAt(0, Runnable.class);
-            // just need to invoke the command once for these tests
-            (new Thread(command)).start();
+            (new Thread(() -> {
+                // just need to invoke the command once for these tests
+                command.run();
+                // notify that the execution is done, so verifications can begin
+                schedulerFixedRateExecutionLatch.countDown();
+              })).start();
             // return value is not used by ContainerHeartbeatMonitor
             return null;
           });
