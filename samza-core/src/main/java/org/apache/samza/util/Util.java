264207f135c8452b8b95d6d52e8eb81ced08cae8
Ke Wu <kwu@linkedin.com>
2020-05-11 15:53:18 -0700

    SAMZA-2523: Update Util#envVarEscape to escape more characters
    Symptom: Job to fail to launch.
    Cause: Backslash and grave accent in submission config is not properly escaped, causing launch.sh to break.
    Changes: Update Util#envVarEscape to escape backslash as well as grave accent.
    Tests: Unit tests updated to test such cases.
    API Changes: None
    Upgrade Instructions: None
    Usage Instructions: None

--- samza-core/src/main/java/org/apache/samza/util/Util.java
+++ samza-core/src/main/java/org/apache/samza/util/Util.java
@@ -45,7 +45,11 @@ public class Util {
    * Make an environment variable string safe to pass.
    */
   public static String envVarEscape(String str) {
-    return str.replace("\"", "\\\"").replace("'", "\\'");
+    return str
+        .replace("\\", "\\\\")
+        .replace("\"", "\\\"")
+        .replace("'", "\\'")
+        .replace("`", "\\`");
   }
 
   public static String getSamzaVersion() {
