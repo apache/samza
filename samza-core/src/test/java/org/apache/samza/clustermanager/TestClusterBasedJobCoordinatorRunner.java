4a7ce75786b3f9d48a511ec7807e6557b82cecad
Ke Wu <kwu@linkedin.com>
2020-09-02 12:46:57 -0700

    Add license info

--- samza-core/src/test/java/org/apache/samza/clustermanager/TestClusterBasedJobCoordinatorRunner.java
+++ samza-core/src/test/java/org/apache/samza/clustermanager/TestClusterBasedJobCoordinatorRunner.java
@@ -1,7 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.samza.clustermanager;
 
 import com.google.common.collect.ImmutableMap;
-import java.io.IOException;
 import org.apache.samza.application.ApplicationUtil;
 import org.apache.samza.application.StreamApplication;
 import org.apache.samza.config.Config;
@@ -11,10 +28,8 @@ import org.apache.samza.config.ShellCommandConfig;
 import org.apache.samza.config.loaders.PropertiesConfigLoaderFactory;
 import org.apache.samza.serializers.model.SamzaObjectMapper;
 import org.apache.samza.util.ConfigUtil;
-import org.apache.samza.util.Util;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
