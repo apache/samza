a8fe3c94d50093131a4772b1be00306ee3495228
mynameborat <bharath.kumarasubramanian@gmail.com>
2020-09-24 11:49:01 -0700

    [Codestyle] Replace the wildcard import with actual imports

--- samza-core/src/test/java/org/apache/samza/task/TestTaskCallbackManager.java
+++ samza-core/src/test/java/org/apache/samza/task/TestTaskCallbackManager.java
@@ -31,7 +31,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 
 public class TestTaskCallbackManager {
