fadc9fc2f804daf1ea33b3681ad837738c7a426d
mynameborat <bharath.kumarasubramanian@gmail.com>
2020-12-07 18:35:00 -0800

    Fix checkstyle

--- samza-core/src/main/java/org/apache/samza/coordinator/JobCoordinatorMetadataManager.java
+++ samza-core/src/main/java/org/apache/samza/coordinator/JobCoordinatorMetadataManager.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.samza.SamzaException;
 import org.apache.samza.config.Config;
 import org.apache.samza.coordinator.stream.CoordinatorStreamValueSerde;
