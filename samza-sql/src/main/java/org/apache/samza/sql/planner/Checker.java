1bf5cf8438f0f85ed4444e9968718b64739ebb6c
Aditya Toomula <atoomula@linkedin.com>
2021-03-22 12:25:54 -0700

    SAMZA-2637: Samza-sql: Fix ANY type validation while checking UDF arg types

--- samza-sql/src/main/java/org/apache/samza/sql/planner/Checker.java
+++ samza-sql/src/main/java/org/apache/samza/sql/planner/Checker.java
@@ -100,9 +100,9 @@ class Checker implements SqlOperandTypeChecker {
         } else if (!Objects.equals(parsedSqlArgType.getSqlTypeName(), udfArgumentAsSqlType)
             && !ANY_SQL_TYPE_NAMES.contains(udfArgumentAsSqlType) && hasOneUdfMethod(udfMetadata)) {
           // 3(b). Throw up and fail on mismatch between the SamzaSqlType and CalciteType for any argument.
-          String msg = String.format("Type mismatch in udf class: %s at argument index: %d." +
-                          "Expected type: %s, actual type: %s.", udfMetadata.getName(),
-              udfArgumentIndex, udfArgumentAsSqlType, parsedSqlArgType.getSqlTypeName());
+          String msg = String.format(
+              "Type mismatch in udf class: %s at argument index: %d." + "Expected type: %s, actual type: %s.",
+              udfMetadata.getName(), udfArgumentIndex, udfArgumentAsSqlType, parsedSqlArgType.getSqlTypeName());
           LOG.error(msg);
           throw new SamzaSqlValidatorException(msg);
         }
