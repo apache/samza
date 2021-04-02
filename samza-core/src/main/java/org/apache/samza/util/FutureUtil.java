e26921c58fa738278dc121748b7c9742713715f3
pmaheshw <pmaheshw@linkedin.com>
2021-04-02 14:35:52 -0700

    Added javadocs and unit tests for FutureUtil.

--- samza-core/src/main/java/org/apache/samza/util/FutureUtil.java
+++ samza-core/src/main/java/org/apache/samza/util/FutureUtil.java
@@ -26,6 +26,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -43,8 +44,49 @@ public class FutureUtil {
   private static final Logger LOG = LoggerFactory.getLogger(FutureUtil.class);
 
   /**
+   * Returns a future that completes when all the futures in the provided collections of futures are complete.
+   * @param futureCollections collections of futures to complete before the returned future is complete
+   */
+  @SafeVarargs
+  public static CompletableFuture<Void> allOf(Collection<? extends CompletionStage<?>>... futureCollections) {
+    List<CompletableFuture<Void>> fvs = new ArrayList<>();
+    for (Collection<? extends CompletionStage<?>> futureCollection : futureCollections) {
+      if (!futureCollection.isEmpty()) {
+        fvs.add(CompletableFuture.allOf(futureCollection.toArray(new CompletableFuture[0])));
+      }
+    }
+
+    return CompletableFuture.allOf(fvs.toArray(new CompletableFuture[0]));
+  }
+
+  /**
+   * Returns a future that completes when all the futures futures are complete.
+   * Returned future completes exceptionally if any future complete with a non-ignored error.
+   */
+  public static CompletableFuture<Void> allOf(Predicate<Throwable> ignoreError, CompletableFuture<?>... futures) {
+    CompletableFuture<Void> allFuture = CompletableFuture.allOf(futures);
+    return allFuture.handle((aVoid, t) -> {
+      for (CompletableFuture<?> future : futures) {
+        try {
+          future.join();
+        } catch (Throwable th) {
+          if (ignoreError.test(th)) {
+            // continue
+          } else {
+            throw th;
+          }
+        }
+      }
+      return null;
+    });
+  }
+
+  /**
    * Helper method to convert: {@code Pair<CompletableFuture<L>, CompletableFuture<R>>}
    * to:                       {@code CompletableFuture<Pair<L, R>>}
+   *
+   * Returns a future that completes when both futures complete.
+   * Returned future completes exceptionally if either of the futures complete exceptionally.
    */
   public static <L, R> CompletableFuture<Pair<L, R>> toFutureOfPair(
       Pair<CompletableFuture<L>, CompletableFuture<R>> pairOfFutures) {
@@ -56,6 +98,9 @@ public class FutureUtil {
   /**
    * Helper method to convert: {@code Map<K, CompletableFuture<V>>}
    * to:                       {@code CompletableFuture<Map<K, V>>}
+   *
+   * Returns a future that completes when all value futures complete.
+   * Returned future completes exceptionally if any of the value futures complete exceptionally.
    */
   public static <K, V> CompletableFuture<Map<K, V>> toFutureOfMap(Map<K, CompletableFuture<V>> keyToValueFutures) {
     return CompletableFuture
@@ -65,56 +110,38 @@ public class FutureUtil {
   }
 
   /**
-   * Helper method to convert: {@code List<CompletableFuture<E>>}
-   * to:                       {@code CompletableFuture<List<E>>}
+   * Helper method to convert: {@code Map<K, CompletableFuture<V>>}
+   * to:                       {@code CompletableFuture<Map<K, V>>}
+   *
+   * Returns a future that completes with successful map entries, skipping any entries with ignored errors,
+   * when all value futures complete.
+   * Returned future completes exceptionally if any of the futures complete with a non-ignored error.
    */
-  public static <E> CompletableFuture<List<E>> toFutureOfList(List<CompletableFuture<E>> futures) {
-    return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-        .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(Collectors.toList()));
-  }
+  public static <K, V> CompletableFuture<Map<K, V>> toFutureOfMap(
+      Predicate<Throwable> ignoreError, Map<K, CompletableFuture<V>> keyToValueFutures) {
+    CompletableFuture<Void> allEntriesFuture =
+        CompletableFuture.allOf(keyToValueFutures.values().toArray(new CompletableFuture[]{}));
 
-  /**
-   * Returns a future that completes when all the futures in the provided collections of futures are complete.
-   * @param futureCollections collections of futures to complete before the returned future is complete
-   */
-  @SafeVarargs
-  public static <V, FV extends CompletionStage<V>> CompletableFuture<Void> allOf(Collection<FV>... futureCollections) {
-    List<CompletableFuture<Void>> fvs = new ArrayList<>();
-    for (Collection<FV> futureCollection : futureCollections) {
-      if (!futureCollection.isEmpty()) {
-        fvs.add(CompletableFuture.allOf(
-            futureCollection.toArray(new CompletableFuture[0])));
+    return allEntriesFuture.handle((aVoid, t) -> {
+      Map<K, V> successfulResults = new HashMap<>();
+      for (Map.Entry<K, CompletableFuture<V>> entry : keyToValueFutures.entrySet()) {
+        K key = entry.getKey();
+        try {
+          V value = entry.getValue().join();
+          successfulResults.put(key, value);
+        } catch (Throwable th) {
+          if (ignoreError.test(th)) {
+            // else ignore and continue
+            LOG.warn("Ignoring value future completion error for key: {}", key, th);
+          } else {
+            throw th;
+          }
+        }
       }
-    }
-
-    return CompletableFuture.allOf(fvs.toArray(new CompletableFuture[0]));
-  }
-
-  public static CompletableFuture<Void> allOf(Predicate<Throwable> ignoreError, CompletableFuture<?>... futures) {
-    CompletableFuture<Void> allFuture = CompletableFuture.allOf(futures);
-    CompletableFuture<Void> resultFuture = new CompletableFuture<>();
-    allFuture.whenComplete((aVoid, t) -> {
-      if (t == null || ignoreError.test(t)) {
-        resultFuture.complete(null);
-      } else {
-        resultFuture.completeExceptionally(t);
-      }
+      return successfulResults;
     });
-    return resultFuture;
   }
 
-  /**
-   * Blocks for a list of futures to complete.
-   * @param futures list of futures to block for
-   */
-  public static <V, FV extends CompletionStage<V>> void joinAll(Collection<FV> futures) {
-    CompletableFuture<Void> completeAllFutures =
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-
-    completeAllFutures.join(); // block and wait for all futures to complete.
-  }
-
-  // TODO BLOCKER shesharm add unit tests
   public static <T> CompletableFuture<T> executeAsyncWithRetries(String opName,
       Supplier<? extends CompletionStage<T>> action,
       Predicate<? extends Throwable> abortRetries,
@@ -137,9 +164,16 @@ public class FutureUtil {
     return cf;
   }
 
-  // Nullable
+  /**
+   * Removes wrapper exceptions of the provided type from the provided throwable and returns the first cause
+   * that does not match the wrapper type. Useful for unwrapping CompletionException / SamzaException
+   * in stack traces and getting to the underlying cause.
+   *
+   * Returns null if provided Throwable is null or if there is no cause of non-wrapper type in the stack.
+   */
   public static <T extends Throwable> Throwable unwrapExceptions(Class<? extends Throwable> wrapperClassToUnwrap, T t) {
     if (t == null) return null;
+    if (wrapperClassToUnwrap == null) return t;
 
     Throwable originalException = t;
     while (wrapperClassToUnwrap.isAssignableFrom(originalException.getClass()) &&
