debbe3e28547a71f163037db034d5462c74fb1b3
Fan Xie <fxie@linkedin.com>
2021-10-08 13:10:46 -0700

    Revert "Try catch error message"
    This reverts commit 4063f175569abfe025cef90239d9af3564600db6.

--- samza-yarn/src/main/scala/org/apache/samza/webapp/ApplicationMasterWebServlet.scala
+++ samza-yarn/src/main/scala/org/apache/samza/webapp/ApplicationMasterWebServlet.scala
@@ -38,14 +38,10 @@ class ApplicationMasterWebServlet(samzaConfig: Config, samzaAppState: SamzaAppli
 
   // Due to AMHA, the uptime and start time of containers (within state) from previous attempt is reset to the time the new AM becomes alive.
   get("/") {
-    try {
-      layoutTemplate("/WEB-INF/views/index.scaml",
-        "config" -> TreeMap(samzaConfig.sanitize.asScala.toMap.toArray: _*),
-        "state" -> state,
-        "samzaAppState" -> samzaAppState,
-        "rmHttpAddress" -> WebAppUtils.getRMWebAppURLWithScheme(yarnConfig))
-    } catch {
-      case e : Exception => println(e)
-    }
+    layoutTemplate("/WEB-INF/views/index.scaml",
+      "config" -> TreeMap(samzaConfig.sanitize.asScala.toMap.toArray: _*),
+      "state" -> state,
+      "samzaAppState" -> samzaAppState,
+      "rmHttpAddress" -> WebAppUtils.getRMWebAppURLWithScheme(yarnConfig))
   }
 }
