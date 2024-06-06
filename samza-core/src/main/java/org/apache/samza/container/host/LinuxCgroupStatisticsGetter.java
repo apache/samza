e9ebd8ae31cc63fd8511867da9e910cc0b0b1f14
Adam Faris <afaris@linkedin.com>
2024-06-05 20:20:14 -0700

    Add unit tests

--- samza-core/src/main/java/org/apache/samza/container/host/LinuxCgroupStatisticsGetter.java
+++ samza-core/src/main/java/org/apache/samza/container/host/LinuxCgroupStatisticsGetter.java
@@ -19,8 +19,9 @@
 
 package org.apache.samza.container.host;
 import java.io.IOException;
+import java.util.Properties;
+import java.io.FileReader;
 import java.nio.file.Files;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -34,26 +35,9 @@ import java.nio.file.Paths;
 
 public class LinuxCgroupStatisticsGetter implements SystemStatisticsGetter {
   private static final Logger LOG = LoggerFactory.getLogger(LinuxCgroupStatisticsGetter.class.getName());
-  private String osName;
   private String cgroupMountPath;
   private String cgroupHierarchy;
   private String containerID;
-
-  LinuxCgroupStatisticsGetter() {
-    this.osName = System.getProperty("os.name");
-    this.containerID = Optional.ofNullable(System.getenv("CONTAINER_ID")).orElse("NOT_DETECTED");
-    String hadoopConfDir =  Optional.ofNullable(System.getenv("HADOOP_CONF_DIR")).orElse("NOT_DETECTED");
-    Configuration yarnSite;
-    if (!hadoopConfDir.equals("NOT_DETECTED")) {
-      yarnSite = getHadoopConf(hadoopConfDir);
-      this.cgroupMountPath = yarnSite.get("yarn.nodemanager.linux-container-executor.cgroups.hierarchy", "NOT_DETECTED");
-      this.cgroupHierarchy = yarnSite.get("yarn.nodemanager.linux-container-executor.cgroups.mount-path", "NOT_DETECTED");
-    }
-    LOG.debug("CONTAINER ID: " + this.containerID);
-    LOG.debug("HADOOP_CONF_DIR: " + hadoopConfDir);
-    LOG.debug("CGROUP_MOUNT_PATH: " + this.cgroupMountPath);
-    LOG.debug("CGROUP_MOUNT_HIERARCHY: " + this.cgroupHierarchy);
-  }
 
   @Override
   public SystemMemoryStatistics getSystemMemoryStatistics() {
@@ -65,45 +49,55 @@ public class LinuxCgroupStatisticsGetter implements SystemStatisticsGetter {
     throw new UnsupportedOperationException("Not implemented");
   }
 
+  LinuxCgroupStatisticsGetter() {
+    this.containerID = Optional.ofNullable(System.getenv("CONTAINER_ID")).orElse("NOT_DETECTED");
+    String hadoopConfDir =  Optional.ofNullable(System.getenv("HADOOP_CONF_DIR")).orElse("NOT_DETECTED");
+    Configuration yarnSite;
+    if (!hadoopConfDir.equals("NOT_DETECTED")) {
+      yarnSite = getHadoopConf(hadoopConfDir);
+      this.cgroupHierarchy = yarnSite.get("yarn.nodemanager.linux-container-executor.cgroups.hierarchy", "NOT_DETECTED");
+      this.cgroupMountPath = yarnSite.get("yarn.nodemanager.linux-container-executor.cgroups.mount-path", "NOT_DETECTED");
+    }
+    LOG.debug("CONTAINER ID: " + this.containerID);
+    LOG.debug("HADOOP_CONF_DIR: " + hadoopConfDir);
+    LOG.debug("CGROUP_MOUNT_PATH: " + this.cgroupMountPath);
+    LOG.debug("CGROUP_MOUNT_HIERARCHY: " + this.cgroupHierarchy);
+  }
+
   @Override
   public LinuxCgroupStatistics getProcessCgroupStatistics() {
-    if (this.osName != "Linux" || this.containerID.equals("NOT_DETECTED")) {
-      LOG.debug("OS is not 'Linux' or Container ID not found in ENV");
+    try {
+      double ratio = getCPUStat();
+      return new LinuxCgroupStatistics(ratio);
+    } catch (Exception e) {
+      LOG.debug("Error reading cgroups information: ", e);
       return null;
     }
-    // TODO: create place holder while we figure this out
-    return new LinuxCgroupStatistics(0.5);
   }
 
   private double getCPUStat() {
-    double cpuThrottleRatio = -1.0;
     String[] controllers = {"cpu", "cpuacct", "cpu,cpuacct" };
+    double cpuThrottledRatio = -1.0;
     String cpuStatPath;
-    Long nrPeriod, nrThrottled = -1L;
     for (String controller : controllers) {
-      cpuStatPath = this.cgroupMountPath + controller + this.cgroupHierarchy + "/cpu.stat";
-      if (cpuStatExists(this.cgroupMountPath + controller + this.cgroupHierarchy + "/cpu.stat")) {
+      cpuStatPath = this.cgroupMountPath + "/" + controller + "/" + this.cgroupHierarchy + "/" + this.containerID + "/cpu.stat";
+      if (cpuStatExists(cpuStatPath)) {
         LOG.debug("Found cpu.stat file: " + cpuStatPath);
-        try (Stream<String> lines = Files.lines(Paths.get(cpuStatPath))) {
-          nrPeriod = Long.parseLong(String.valueOf(lines.map(line -> line.split(" "))
-              .filter(line -> line.length == 2 && line[0].equals("nr_periods"))
-              .map(line -> line[1])
-              .findFirst())
-          );
-          nrThrottled = Long.parseLong(String.valueOf(lines.map(line -> line.split(" "))
-              .filter(line -> line.length == 2 && line[0].equals("nr_throttled"))
-              .map(line -> line[1])
-              .findFirst())
-          );
-          LOG.debug("cpu.stat nr_period value: " + nrPeriod.toString());
-          LOG.debug("cpu.stat nr_throttled value: " + nrThrottled.toString());
-          cpuThrottleRatio = nrPeriod / nrThrottled;
-        } catch (RuntimeException | IOException e) {
-          LOG.error("Unable to read cpu.stat file: {}", e.getStackTrace());
+        try {
+          Properties cpuStatValues = new Properties(); // Treat cpu.stat as a properties file as content is space delimited key value data.
+          cpuStatValues.load(new FileReader(cpuStatPath));
+          long nrPeriod = Long.parseLong(cpuStatValues.getProperty("nr_periods", "-1.0"));
+          long nrThrottled = Long.parseLong(cpuStatValues.getProperty("nr_throttled", "-1.0"));
+          LOG.debug("cpu.stat nr_period value: " + nrPeriod);
+          LOG.debug("cpu.stat nr_throttled value: " + nrThrottled);
+          cpuThrottledRatio = (double) nrThrottled / nrPeriod;
+          break;
+        } catch (IOException | RuntimeException e) {
+          throw new RuntimeException("Caught exception reading cpu.stat file: ", e);
         }
       }
     }
-    return cpuThrottleRatio;
+    return cpuThrottledRatio;
   }
 
   private boolean cpuStatExists(String cpuStatPath) {
@@ -124,4 +118,5 @@ public class LinuxCgroupStatisticsGetter implements SystemStatisticsGetter {
     return hConf;
   }
 }
+
 
--- samza-core/src/test/java/org/apache/samza/container/host/TestLinuxCgroupStatisticsGetter.java
+++ samza-core/src/test/java/org/apache/samza/container/host/TestLinuxCgroupStatisticsGetter.java
@@ -19,19 +19,79 @@
 
 package org.apache.samza.container.host;
 
-import org.junit.Test;
+
 import static org.junit.Assert.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import java.util.logging.Logger;
+import org.junit.Assume;
 
 
 public class TestLinuxCgroupStatisticsGetter {
-// TODO add tests
+  private static final Logger LOGGER = Logger.getLogger("TestLinuxCgroupStatisticsGetter");
+
+  @Rule
+  public TemporaryFolder myTempDir = new TemporaryFolder();
+
+  @Rule
+  public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
   @Test
-  public void placeHolderTest() {
-    assertTrue(true);
+  public void testGetThrottleValue() {
+    Assume.assumeTrue(System.getProperty("os.name").equals("Linux"));
+    environmentVariables.set("CONTAINER_ID", "container_abc_123");
+    environmentVariables.set("HADOOP_CONF_DIR", myTempDir.getRoot().toString());
+
+    File yarnSiteXml, cpuStatFile, cpuStatDirs;
+    try {
+      // Need stub data at my_temp_dir/yarn-site.xml
+      //                   my_temp_dir/cgroup/cpu/user.slice/container_abc_123/cpu.stat
+      yarnSiteXml = myTempDir.newFile("yarn-site.xml");
+
+      FileWriter fw1 = new FileWriter(yarnSiteXml);
+      BufferedWriter bw1 = new BufferedWriter(fw1);
+      bw1.write("<configuration><property>");
+      bw1.write("<name>yarn.nodemanager.linux-container-executor.cgroups.hierarchy</name>");
+      bw1.write("<value>user.slice</value></property>");
+      bw1.write("<property><name>yarn.nodemanager.linux-container-executor.cgroups.mount-path</name>");
+      bw1.write("<value>" + myTempDir.getRoot() + "/cgroup</value>");
+      bw1.write("</property></configuration>");
+      bw1.close();
+
+      String cpuStatPath = myTempDir.getRoot() + "/cgroup/cpu/user.slice/container_abc_123";
+      cpuStatDirs = new File(cpuStatPath);
+      cpuStatDirs.mkdirs();
+
+      cpuStatFile = new File(cpuStatDirs, "cpu.stat");
+      cpuStatFile.createNewFile();
+
+      FileWriter fw2 = new FileWriter(cpuStatFile);
+      BufferedWriter bw2 = new BufferedWriter(fw2);
+      bw2.write("nr_periods 340956467");
+      bw2.newLine();
+      bw2.write("nr_throttled 292501");
+      bw2.newLine();
+      bw2.write("throttled_time 5997018459867");
+      bw2.newLine();
+      bw2.close();
+
+    } catch (IOException e) {
+      System.err.println("Error creating temporary test files: " + e.getMessage());
+    }
+
+    LinuxCgroupStatisticsGetter linuxCgroupStatisticsGetter = new LinuxCgroupStatisticsGetter();
+    LinuxCgroupStatistics cpuStat = linuxCgroupStatisticsGetter.getProcessCgroupStatistics();
+
+    double throttleRatio = cpuStat.getCgroupCpuThrottleRatio();
+    assertEquals(throttleRatio, 0.00085788371334807384, 0.05);
+
   }
-
-
-
 
   @Test(expected = UnsupportedOperationException.class)
   public void testGetSystemMemoryStatistics() {
